107a2612c65055eaa2ee4b86210e5e30c81a5191
Leo Brodeur <leobrod44@gmail.com>
2024-10-03 23:46:56 -0400

    config changes

--- server/main.go
+++ server/main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	rabbitmq "Smartess/server/rabbit-mq"
 	"log"
 	"os"
-	"sync"
 	"time"
 )
 
 func main() {
+
 	// Open log file
 	f, err := os.OpenFile("/app/logs/server.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
@@ -17,14 +16,8 @@ func main() {
 	defer f.Close()
 	log.SetOutput(f)
 
-	// Logging message
-	log.Println("This is a log message!")
-
-	var wg sync.WaitGroup
-	rabbitmq.StartConsumer(&wg)
-	// Run indefinitely with a sleep
 	for {
 		time.Sleep(10 * time.Second) // Sleep for 1 second
-		log.Println("Still running...")
+		log.Println("Server still running...")
 	}
 }
