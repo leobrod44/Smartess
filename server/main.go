755e958219fa77d299c913eec138026cc665a63d
Leo Brodeur <leobrod44@gmail.com>
2024-10-10 21:47:30 -0400

    producer

--- server/main.go
+++ server/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"smartess/server/rabbitmq"
 	"time"
+
+	"github.com/streadway/amqp"
 )
 
 func main() {
@@ -17,10 +20,64 @@ func main() {
 
 	srv.Logger.Info("Logger initialized and started")
 
+	//Start the mock message producer
+
+	go StartProducer()
+
+	// Start the RabbitMQ server (consumer)
 	for {
 		go srv.Start()               // Start the RabbitMQ server on a separate goroutine
 		time.Sleep(10 * time.Second) // Sleep for activity monitoring, not this dosen't mean the server stops since we used a goroutine!
 		//TODO print still running for starting purposes, but removing this useless clutter down the line
 		log.Println("Server still running...")
 	}
+}
+
+func StartProducer() {
+	// Connect to RabbitMQ
+	conn, ch := ConnectToRabbitMQ("amqp://guest:guest@localhost:5672/")
+	defer ch.Close()
+	defer conn.Close()
+	q, err := ch.QueueDeclare(
+		"test_queue", // name
+		true,         // durable (will persist through restarts)
+		false,        // delete when unused
+		false,        // exclusive
+		false,        // no-wait
+		nil,          // arguments
+	)
+	if err != nil {
+		log.Fatalf("Failed to declare a queue: %v", err)
+	}
+	// Send a message every second
+	for {
+		body := fmt.Sprintf("Current time: %s", time.Now().Format(time.RFC3339))
+		err = ch.Publish(
+			"",     // exchange
+			q.Name, // routing key
+			false,  // mandatory
+			false,  // immediate
+			amqp.Publishing{
+				ContentType: "text/plain",
+				Body:        []byte(body),
+			})
+		if err != nil {
+			log.Fatalf("Failed to publish a message: %v", err)
+		}
+		log.Printf(" [x] Sent %s", body)
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func ConnectToRabbitMQ(uri string) (*amqp.Connection, *amqp.Channel) {
+	conn, err := amqp.Dial(uri)
+	if err != nil {
+		log.Fatalf("Failed to connect to RabbitMQ: %v", err)
+	}
+	defer conn.Close()
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("Failed to open a channel: %v", err)
+	}
+	return conn, ch
 }
