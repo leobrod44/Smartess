c5890710735ce31717cbbc444572c4c1ebd5eb74
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2024-10-12 11:03:56 -0400

    implementing form validation using react toastify. checking empty fields and email format #35

--- smartessweb/src/app/sign-in/page.tsx
+++ smartessweb/src/app/sign-in/page.tsx
@@ -1,9 +1,38 @@
+"use client"
 import Image from 'next/image';
 import building_straight from '../../public/images/building_straight.png';
+import { useState } from 'react';
+import Toast, { showToastError, showToastSuccess } from '../components/Toast';
 
 const SignInPage = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const validateEmail = (email: string) => {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Basic email regex
+    return emailPattern.test(email);
+  };
+
+  const handleLogin = () => {
+    if (!email || !password) {
+      showToastError('Please fill in all required fields');
+    }
+    else if (!validateEmail(email)) {
+      showToastError('Please enter a valid email address');
+      return;
+    } 
+    else
+    {
+      // Proceed with login logic
+      showToastSuccess('Login successful!');
+    }
+  };
+
+
   return (
     <div className="flex h-screen bg-white">
+    <Toast />
 
     {/* Left side content */}
     <div className=" w-full md:w-1/2 flex flex-col justify-center items-center p-5">
@@ -23,8 +52,14 @@ const SignInPage = () => {
         <div className="self-stretch px-2.5  justify-start items-center gap-2.5 inline-flex">
           <div className="text-[#266472] text-[20px] font-sequel-sans-regular">Email</div>
         </div>
-        <div className="self-stretch px-5 py-3 bg-[#898888]/20 rounded-[20px] justify-start items-center gap-2.5 inline-flex">
-          <div className="w-[148px] text-[#266472]/20 text-xl font-sequel-sans-regular">Your email</div>
+        <div className="self-stretch">
+          <input
+          type= "email"
+          placeholder='Your email'
+          className="w-full text-[#266472] text-xl font-sequel-sans-regular self-stretch px-5 py-3 bg-[#898888]/20 rounded-[20px] items-center gap-2.5 inline-flex focus:outline-none"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
       </div>
 
@@ -33,23 +68,34 @@ const SignInPage = () => {
         <div className="self-stretch px-2.5 justify-start items-center gap-2.5 inline-flex">
           <div className="text-[#266472] text-[20px] font-sequel-sans-regular">Password</div>
         </div>
-        <div className="self-stretch px-5 py-3 bg-[#898888]/20 rounded-[20px] justify-start items-center gap-2.5 inline-flex">
-          <div className="text-[#266472]/20 text-xl font-sequel-sans-regular">Your Password</div>
+        <div className="self-stretch">
+        <input
+           type="password"
+           placeholder="Your Password"
+           className=" w-full self-stretch px-5 py-3 bg-[#898888]/20 rounded-[20px] text-[#266472] text-xl font-sequel-sans-regular focus:outline-none"
+           value={password}
+           onChange={(e) => setPassword(e.target.value)}
+         />
         </div>
       </div>
 
       {/* Forgot password link */}
       <div className="pl-[266px] justify-end items-center inline-flex pt-10 ">
-        <div className="text-center text-[#266472]/40 text-xl font-light font-sequel-sans-light underline hover:text-[#30525e] custom-transition-length-1s">
+        <a 
+        href="/forgot-password/path"
+        className="text-center text-[#266472]/40 text-xl font-light font-sequel-sans-light underline hover:text-[#30525e] custom-transition-length-1s">
           Forgot your password?
-        </div>
+        </a>
       </div>
 
       {/* Login button */}
       <div className='h-[102px] w-full max-w-lg py-5 flex flex-col justify-center items-center gap-2.5 flex '>
-        <div className="self-stretch px-[149px] py-[13px] bg-[#30525e] opacity-40 rounded-[20px] shadow justify-center items-center gap-2.5 inline-flex transition-opacity hover:opacity-100 custom-transition-length-1s ">
-          <div className="text-center text-white text-lg font-sequel-sans-regular">Login</div>
-        </div>
+        <button 
+        className=" self-stretch px-[149px] py-[13px] bg-[#30525e] opacity-40 rounded-[20px] shadow justify-center items-center gap-2.5 inline-flex transition-opacity hover:opacity-100 custom-transition-length-1s text-center text-white text-lg font-sequel-sans-regular"
+        onClick={handleLogin} 
+        >
+          Login
+        </button>
       </div>
     </div>
 
