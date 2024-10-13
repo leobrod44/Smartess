ee8e03f10cde57d274884ebb579c099a205c121a
Tuan Anh Pham <98975548+TuanAnh-P@users.noreply.github.com>
2024-10-13 16:41:41 -0400

    US2.1: Login Page (#53)
    * Backend user auth with Supabase, connect login page to backend api #12
    
    * Fixed eslint error #12

---
+++ smartessweb/src/app/dashboard/page.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+const DashboardPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      router.push('/sign-in');
+    }
+  }, [router]);
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  );
+};
+
+export default DashboardPage;
