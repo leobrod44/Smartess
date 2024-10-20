be33f877cf818bdd191eb07c3d37bcc6b11e0035
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2024-10-20 17:52:28 -0400

    WIP--> Adjusting CSS to use more responsive spacing values #72

--- smartessweb/frontend/src/app/components/UnitComponents/HubOwner.tsx
+++ smartessweb/frontend/src/app/components/UnitComponents/HubOwner.tsx
@@ -13,20 +13,20 @@ const HubOwner = ({
   };
 
   return (
-    <div className="w-[246px] h-[173px] px-[15px] py-2.5 bg-white flex-col justify-start items-center gap-2.5 inline-flex">
-      <div className="w-[162px] h-[47px] relative bg-transparent">
-        <div className="w-[132px] h-[33px] left-[9px] top-0 absolute text-center text-[#4b7d8d] text-xl font-['Sequel Sans'] leading-tight tracking-tight">
+    <div className="max-w-xs p-4 flex flex-col items-center gap-2.5 shadow-md">
+      <div className="w-full relative pb-2.5">
+        <div className="text-center text-[#4b7d8d] text-xl font-['Sequel Sans'] leading-tight tracking-tight">
           Hub owner
         </div>
-        <div className="w-36 h-px left-[9px] top-[33px] absolute bg-[#4b7d8d]"></div>
+        <div className="w-full h-px absolute bg-[#4b7d8d]"></div>
       </div>
-      <div className="w-[244px] h-[17px] text-center text-black text-sm font-['Sequel Sans'] leading-tight tracking-tight">
+      <div className="text-center text-black text-sm font-['Sequel Sans'] leading-tight tracking-tight">
         {formatName(owner.firstName)} {formatName(owner.lastName)}
       </div>
-      <div className="w-[245px] text-center text-black text-sm font-['Sequel Sans'] leading-tight tracking-tight">
+      <div className="text-center text-black text-sm font-['Sequel Sans'] leading-tight tracking-tight">
         {owner.email}
       </div>
-      <button className="w-[78px] h-[23px] px-0.5 bg-[#4b7d8d] rounded-[10px] justify-center items-center gap-5 inline-flex hover:bg-[#1f505e] transition duration-300 text-center text-white text-xs font-['Sequel Sans'] leading-tight tracking-tight">
+      <button className="mt-3 w-[80px] h-[22px] bg-[#4b7d8d] rounded-md hover:bg-[#1f505e] transition duration-300 text-white text-xs font-['Sequel Sans'] leading-tight tracking-tight">
         Contact
       </button>
     </div>
