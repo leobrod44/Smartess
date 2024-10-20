8e39c2c4cec7da76bfa75708ef69f2c373179dab
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2024-10-20 17:52:35 -0400

    WIP--> Adjusting CSS to use more responsive spacing values #72

--- smartessweb/frontend/src/app/components/UnitComponents/Tickets.tsx
+++ smartessweb/frontend/src/app/components/UnitComponents/Tickets.tsx
@@ -3,41 +3,41 @@ const Tickets = ({
 }: {
   tickets: { total: number; pending: number; open: number; closed: number };
 }) => (
-  <div className="w-[267px] h-[164px] px-[13px] bg-white flex-col justify-start items-center inline-flex">
-    <div className="w-[162px] h-[37px] relative bg-white/0">
-      <div className="w-[173px] h-[33px] absolute text-center text-[#4b7d8d] text-xl font-['Sequel Sans'] leading-tight tracking-tight">
+  <div className="max-w-xs p-4 bg-white flex flex-col justify-start items-center shadow-md">
+    <div className="w-full relative pb-2.5">
+      <div className="text-center text-[#4b7d8d] text-xl font-['Sequel Sans'] leading-tight tracking-tight">
         Tickets
       </div>
-      <div className="w-36 h-px left-[9px] top-[32px] absolute bg-[#4b7d8d]" />
+      <div className="w-full h-px absolute bg-[#4b7d8d]" />
     </div>
 
-    <div className="w-full h-[127px] grid grid-cols-2 gap-[7px] px-[9px] py-[7px]">
-      <div className="h-[50px] px-0.5 bg-[#4b7d8d] rounded-[10px] flex justify-center items-center">
-        <div className="text-center text-white text-base font-['Sequel Sans'] leading-tight tracking-tight">
+    <div className="w-full grid grid-cols-2 gap-2">
+      <div className="bg-[#4b7d8d] rounded-[10px] flex justify-center items-center p-2">
+        <div className="text-center text-white text-sm font-['Sequel Sans'] leading-tight tracking-tight">
           {tickets.total}
           <br />
           Total
         </div>
       </div>
 
-      <div className="h-[50px] px-0.5 bg-[#729987] rounded-[10px] flex justify-center items-center">
-        <div className="text-center text-white text-base font-['Sequel Sans'] leading-tight tracking-tight">
+      <div className="bg-[#729987] rounded-[10px] flex justify-center items-center p-2">
+        <div className="text-center text-white text-sm font-['Sequel Sans'] leading-tight tracking-tight">
           {tickets.open}
           <br />
           Open
         </div>
       </div>
 
-      <div className="h-[50px] px-0.5 bg-[#a6634f] rounded-[10px] flex justify-center items-center">
-        <div className="text-center text-white text-base font-['Sequel Sans'] leading-tight tracking-tight">
+      <div className="bg-[#a6634f] rounded-[10px] flex justify-center items-center p-2">
+        <div className="text-center text-white text-sm font-['Sequel Sans'] leading-tight tracking-tight">
           {tickets.pending}
           <br />
           Pending
         </div>
       </div>
 
-      <div className="h-[50px] px-0.5 bg-[#cccccc] rounded-[10px] flex justify-center items-center">
-        <div className="text-center text-white text-base font-['Sequel Sans'] leading-tight tracking-tight">
+      <div className="bg-[#cccccc] rounded-[10px] flex justify-center items-center p-2">
+        <div className="text-center text-white text-sm font-['Sequel Sans'] leading-tight tracking-tight">
           {tickets.closed}
           <br />
           Closed
