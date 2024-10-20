3b2919dbba6848c060be7123a687ffac514fcce5
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2024-10-20 17:52:43 -0400

    WIP--> Adjusting CSS to use more responsive spacing values #72

--- smartessweb/frontend/src/app/components/UnitComponents/Alerts.tsx
+++ smartessweb/frontend/src/app/components/UnitComponents/Alerts.tsx
@@ -1,33 +1,38 @@
-
 const Alerts = ({ alerts }: { alerts: { message: string }[] }) => {
-    // Show only the first 2 alerts
-    const firstTwoAlerts = alerts.slice(0, 2);
-  
-    return (
-      <div className="w-[252px] h-[165px] px-[23px] bg-white/0 flex-col justify-start items-center inline-flex">
-        <div className="h-[34px] px-[9px] bg-white/0 flex-col justify-center items-center flex">
-          <h3 className="self-stretch h-[33px] text-center text-[#4b7d8d] text-xl font-['Sequel Sans'] leading-tight tracking-tight">
-            Alerts
-          </h3>
-          <div className="self-stretch h-px bg-[#4b7d8d]" />
-        </div>
-  
-        <div className="py-[15px] bg-white/0 flex-col justify-center items-center gap-2.5 flex">
-          {firstTwoAlerts.map((alert, index) => (
-            <div key={index} className="h-[22px] px-3 py-[5px] bg-[#a65146] rounded-[5px] flex justify-center items-center">
-              <div className="text-center text-white text-[9px] font-['Sequel Sans'] leading-[17.84px] tracking-tight">
-                {alert.message}
-              </div>
+  // Show only the first 2 alerts
+  const firstTwoAlerts = alerts.slice(0, 2);
+
+  const capitalizeMessage = (message: string) => {
+    return message.toUpperCase();
+  };
+
+  return (
+    <div className="max-w-xs p-4 bg-white flex-col justify-start items-center shadow-md">
+      <div className="w-full relative pb-2.5">
+        <h3 className="text-center text-[#4b7d8d] text-xl font-['Sequel Sans'] leading-tight tracking-tight">
+          Alerts
+        </h3>
+        <div className="w-full h-px absolute bg-[#4b7d8d]" />
+      </div>
+
+      <div className="flex flex-col justify-center items-center gap-2.5">
+        {firstTwoAlerts.map((alert, index) => (
+          <div
+            key={index}
+            className="w-[150px] h-[22px] bg-[#a65146] rounded-md flex justify-center items-center overflow-hidden"
+          >
+            <div className="text-center text-white text-[9px] font-['Sequel Sans'] tracking-tight whitespace-nowrap overflow-ellipsis overflow-hidden">
+              {capitalizeMessage(alert.message)}
             </div>
-          ))}
-        </div>
-  
-        <button className="w-[82px] h-[22px] px-[1.19px] bg-[#a65146] rounded-md text-white text-xs">
+          </div>
+        ))}
+
+        <button className="mt-3 w-[80px] h-[22px] bg-[#a65146] rounded-md text-white text-xs hover:bg-[#ff5449] transition duration-300">
           See All
         </button>
       </div>
-    );
-  };
-  
-  export default Alerts;
-  
+    </div>
+  );
+};
+
+export default Alerts;
