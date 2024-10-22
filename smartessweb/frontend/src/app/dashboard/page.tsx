e4a989070a6be3a9ed12a1e76139ccb2c8f572a4
layanat <tounsilayana@gmail.com>
2024-10-22 14:00:30 -0400

    Added dashboard page layout and rendered project component  #71 #15

--- smartessweb/frontend/src/app/dashboard/page.tsx
+++ smartessweb/frontend/src/app/dashboard/page.tsx
@@ -1,21 +1,31 @@
-'use client';
+"use client";
 
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import ProjectComponent from "../components/ProjectComponent";
 
 const DashboardPage = () => {
   const router = useRouter();
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (!token) {
-      router.push('/sign-in');
+      router.push("/sign-in");
     }
   }, [router]);
 
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className="border border-black rounded-lg p-6 mx-4 lg:mx-8 mt-6 min-h-screen flex flex-col">
+      <div className="text-left text-[#325a67] text-[30px] leading-10 tracking-tight">
+        Welcome to Your Dashboard
+      </div>
+
+      {/* Widgets go here */}
+
+      <div className=" pt-4 w-[306px] h-[66px] text-[#325a67] text-[30px]  leading-10 tracking-tight">
+        Your Projects
+      </div>
+      <ProjectComponent />
     </div>
   );
 };
