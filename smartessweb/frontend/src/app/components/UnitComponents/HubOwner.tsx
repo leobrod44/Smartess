6382ef2b0089bfc6920e9ddb55be0f749ae8e252
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2024-10-23 14:24:17 -0400

    US5.2 TASK 4 implementing the Unit Component (#91)
    * Setting up iniital files for the unit components #72
    
    * WIP-->initial styling for ticket component #72
    
    * WIP-->initial styling for hub users list component #72
    
    * WIP-->initial styling for hub owner component #72
    
    * WIP-->initial styling for alert component #72
    
    * WIP-->initial styling for whole unit component #72
    
    * Adding a method in hubowner to format the first name and last name #72
    
    * fixing incorrect import in unit component #72
    
    * fixing formatting #72
    
    * WIP--> Adjusting CSS to use more responsive spacing values #72
    
    * WIP--> Adjusting CSS to use more responsive spacing values #72
    
    * WIP--> Adjusting CSS to use more responsive spacing values #72
    
    * WIP--> Adjusting CSS to use more responsive spacing values #72
    
    * Adjusting the shadows of the components #72
    
    * WIP--> css of entire unit component. #72
    
    * Adjusting font sizes and spacing to reduce overall size of components #72
    
    * Making Unit responsive, so that on small screens it appears vertical #72
    
    * adjusting onhover effect color for the alerts #72
    
    * simulating grabbing data from backend #72
    
    * Added a condition to check if there are no alerts- if so, display a "no alerts" msg #72
    
    * Added a condition to check if there is no hub owner- if so, display a no hub owner message #72
    
    * removing unused import caught by linter #72
    
    * Adding bolder fonts for the titles #72

---
+++ smartessweb/frontend/src/app/components/UnitComponents/HubOwner.tsx
@@ -0,0 +1,48 @@
+const HubOwner = ({
+  owner,
+}: {
+  owner: { firstName: string; lastName: string; email: string } | null;
+}) => {
+  //Formatting method to ensure the names have first letter capitalization only
+  const formatName = (name: string) => {
+    return name
+      .toLowerCase()
+      .split(" ")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
+  };
+
+  // Check if the owner exists and has valid properties
+  const hasOwner = owner && owner.firstName && owner.lastName && owner.email;
+
+  return (
+    <div className="max-w-xs p-4 flex flex-col items-center gap-2.5 pr-1">
+      <div className="w-full relative pb-2.5">
+        <div className="text-center text-[#4b7d8d] text-l font-sequel-sans-black leading-tight tracking-tight">
+          Hub owner
+        </div>
+        <div className="w-full h-px absolute bg-[#4b7d8d]"></div>
+      </div>
+
+      {hasOwner ? (
+        <>
+          <div className="text-center text-black text-xs font-['Sequel Sans'] leading-tight tracking-tight">
+            {formatName(owner.firstName)} {formatName(owner.lastName)}
+          </div>
+          <div className="text-center text-black text-xs font-['Sequel Sans'] leading-tight tracking-tight">
+            {owner.email}
+          </div>
+          <button className="w-[80px] h-[22px] bg-[#4b7d8d] rounded-md hover:bg-[#1f505e] transition duration-300 text-white text-xs font-['Sequel Sans'] leading-tight tracking-tight">
+            Contact
+          </button>
+        </>
+      ) : (
+        <div className="text-center text-black text-xs font-['Sequel Sans'] leading-tight tracking-tight">
+          No hub owner found
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default HubOwner;
