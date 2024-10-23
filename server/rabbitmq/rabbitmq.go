46daf0f80a7cc61f09e4227599fdfac809ce030b
Ryan Li <99624359+Ryan2Li@users.noreply.github.com>
2024-10-23 11:08:24 -0400

    Task mongodb setup (#62)
    * added test mongo producer and consumer
    
    * temp comment out mongo producers and consumers

--- server/rabbitmq/rabbitmq.go
+++ server/rabbitmq/rabbitmq.go
@@ -1,12 +1,19 @@
 package rabbitmq
 
 import (
+	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/streadway/amqp"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"log"
 	"os"
+	"smartess/server/rabbitmq/messages"
 )
 
 // RabbitMQServer represents a RabbitMQ server instance.
@@ -64,8 +71,23 @@ func Init() (RabbitMQServer, error) {
 	if err != nil {
 		logger.Fatal("ERROR: Failed to declare queue", zap.Error(err))
 	}
+
+	mongoQueue, err := Declare(ch, QueueConfig{
+		"mongo-queue",
+		false,
+		true,
+		false,
+		false,
+		false,
+		nil,
+	})
+
+	if err != nil {
+		logger.Fatal("ERROR: Failed to declare queue", zap.Error(err))
+	}
+
 	srv := RabbitMQServer{
-		smartessQueues: []SmartessQueue{sampleQueue},
+		smartessQueues: []SmartessQueue{sampleQueue, mongoQueue},
 		Logger:         logger,
 		channel:        ch,
 		connection:     conn,
@@ -128,4 +150,75 @@ func InitializeLogger() (*zap.Logger, error) {
 
 	logger := zap.New(core, zap.AddCaller(), zap.AddStacktrace(zapcore.ErrorLevel))
 	return logger, nil
+}
+
+var mongoClient *mongo.Client
+
+// ConnectToMongo initializes the MongoDB client.
+func ConnectToMongo() {
+	clientOptions := options.Client().ApplyURI("mongodb+srv://cluster0admin:cluster0admin@cluster0.yko5a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+	var err error
+	mongoClient, err = mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatalf("Failed to connect to MongoDB: %v", err)
+	}
+
+	// Check the connection
+	err = mongoClient.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatalf("Failed to ping MongoDB: %v", err)
+	}
+	fmt.Println("Connected to MongoDB!")
+}
+
+func (r *RabbitMQServer) StartMongoConsumer() {
+
+	ConnectToMongo()
+
+	defer r.connection.Close()
+	defer r.channel.Close()
+
+	for _, smartessQueue := range r.smartessQueues {
+		go func(queue SmartessQueue) {
+			msgs, err := r.channel.Consume(
+				"mongo-queue",
+				"",
+				true,
+				false,
+				false,
+				false,
+				nil,
+			)
+			if err != nil {
+				r.Logger.Error("Failed to consume messages", zap.Error(err))
+			}
+			for msg := range msgs {
+				var message messages.MongoMessage
+				err := json.Unmarshal(msg.Body, &message)
+				if err != nil {
+					log.Printf("Failed to unmarshal message: %v", err)
+					continue
+				}
+
+				// Insert the message into MongoDB
+				collection := mongoClient.Database("TestDB1").Collection("test")
+				_, err = collection.InsertOne(context.TODO(), bson.D{
+					{"_id", message.ID},
+					{"content", message.Content},
+					{"timestamp", message.Timestamp},
+				})
+				if err != nil {
+					log.Printf("Failed to insert message into MongoDB: %v", err)
+					continue
+				}
+
+				fmt.Printf("Inserted message into MongoDB: %+v\n", message)
+			}
+		}(smartessQueue)
+	}
+
+	// Wait indefinitely (this will run after the goroutines above since go funcs are async (they start a thread and run concurrently))
+	r.Logger.Info("RabbitMQ server started. Waiting for messages...")
+	select {}
 }
