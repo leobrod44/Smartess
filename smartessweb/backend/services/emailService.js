4e83e820dfdb5bffa34037c99f40a3275ea7e8eb
Matthew Flaherty <matthewflaherty77@hotmail.com>
2024-10-25 20:40:26 -0400

    new function for writing to db

--- smartessweb/backend/services/emailService.js
+++ smartessweb/backend/services/emailService.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const { Resend } = require('resend');
+const supabase = require('../config/supabase');
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -19,7 +20,32 @@ exports.sendEmail = async (from, to, subject, htmlContent) => {
 
     return { success: true, data };
   } catch (err) {
-    console.error('Server error sending email:', err);
-    return { success: false, error: err };
+      console.error('Server error sending email:', err);
+      return { success: false, error: err };
+  }
+};
+
+exports.storeEmail = async (businessName, firstName, lastName, telephoneNumber, email, description) => {
+  try {
+    const { data, error } = await supabase
+    .from('start_project')
+    .insert([{
+      business_name: businessName,   
+      first_name: firstName,        
+      last_name: lastName,     
+      email,
+      phone_number: telephoneNumber,
+      description
+    }]);
+
+    if (error) {
+      console.error('Error storing email:', error);
+      return { success: false, error };
+    }
+
+    return { success: true, data };
+  } catch (err) {
+      console.error('Server error storing email:', err);
+      return { success: false, error: err };
   }
 };
