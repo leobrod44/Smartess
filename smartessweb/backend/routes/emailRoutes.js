91efce96abf23d445bd9a793d7637d70e2488996
Matthew Flaherty <matthewflaherty77@hotmail.com>
2024-10-25 21:26:35 -0400

    removed email files

--- smartessweb/backend/routes/emailRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { sendEmailController, storeEmailController } = require('../controllers/emailController');
-const router = express.Router();
-
-router.post('/send-email', sendEmailController);
-router.post('/store-email', storeEmailController);
-
-module.exports = router;
