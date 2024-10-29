ca29b88a5f2fec7612a13d2661d6bad24e40717d
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2024-10-29 15:40:45 -0400

    Bug-hotfix added missing prop companyId for DashboardWidget

--- smartessweb/frontend/src/app/dashboard/page.tsx
+++ smartessweb/frontend/src/app/dashboard/page.tsx
@@ -20,7 +20,7 @@ const DashboardPage = () => {
       <div className="text-left text-[#325a67] text-[30px] leading-10 tracking-tight pb-4">
         Welcome to Your Dashboard
       </div>
-      <DashboardWidget />
+      <DashboardWidget companyId="12345" />
       <div className=" pt-4 w-[306px] h-[66px] text-[#325a67] text-[30px]  leading-10 tracking-tight">
         Your Projects
       </div>
