c4f5ab7cf0743683eff655c6fd73d31c5eeab2d8
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2024-10-29 13:12:47 -0400

    - TASK--6-US-5.1 Padding added in DashboardOverviewWidget when responsive  #90

--- smartessweb/frontend/src/app/components/DashboardComponents/SystemOverviewWidget.tsx
+++ smartessweb/frontend/src/app/components/DashboardComponents/SystemOverviewWidget.tsx
@@ -13,22 +13,22 @@ const DashboardOverviewWidget = ({
       <div className="flex flex-row w-full h-2/4 p-1">
         <div className="bg-[#CCCCCC] w-full h-full place-content-around rounded-[7px] m-0.5">
           <h3 className="text-2xl">{systemOverview?.projects}</h3>
-          <h3 className="text-xs">Projects</h3>
+          <h3 className="text-xs pb-1">Projects</h3>
         </div>
         <div className="bg-[#4B7D8D] w-full h-full place-content-around rounded-[7px] m-0.5">
           <h3 className="text-2xl">{systemOverview?.totalUnits}</h3>
-          <h3 className="text-xs">Total Units</h3>
+          <h3 className="text-xs pb-1 ">Total Units</h3>
         </div>
       </div>
 
       <div className="flex flex-row h-2/4 w-full p-1">
         <div className="bg-[#A6634F] w-full h-full place-content-around rounded-[7px] m-0.5">
           <h3 className="text-2xl">{systemOverview?.pendingTickets}</h3>
-          <h3 className="text-xs">Pending Tickets</h3>
+          <h3 className="text-xs pb-1">Pending Tickets</h3>
         </div>
         <div className="bg-[#729987] w-full h-full place-content-around rounded-[7px] m-0.5">
           <h3 className="text-2xl">{systemOverview?.totalAdminUsers}</h3>
-          <h3 className="text-xs">Total Admin Users</h3>
+          <h3 className="text-xs pb-1">Total Admin Users</h3>
         </div>
       </div>
     </div>
