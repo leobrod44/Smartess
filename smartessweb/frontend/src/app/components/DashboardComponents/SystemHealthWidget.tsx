3f139b8811146ab924a1cb8948ddf8b704ad8d8a
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2024-10-29 15:21:44 -0400

    US5.1 TASK 6 Implement the dashboard widgets (#116)
    * TASK--6-US-5.1 implementing dashboard widgets responsive design #90
    
    * TASK--6-US-5.1 implementing dashboard widgets with mock data #90
    
    * TASK--6-US-5.1 implementing dashboard  for first 2 recent alerts displayed #90
    
    * TASK--6-US-5.1 fixing nomenclature for MockDashboard #90
    
    * TASK--6-US-5.1 Padding added in DashboardOverviewWidget when responsive  #90

---
+++ smartessweb/frontend/src/app/components/DashboardComponents/SystemHealthWidget.tsx
@@ -0,0 +1,31 @@
+const SystemHealthWidget = ({
+  systemHealth,
+}: {
+  systemHealth: {
+    systemsLive: number;
+    systemsDown: number;
+  } | null;
+}) => {
+  return (
+    <div className="flex-col items-center bg-[#325A67] w-full rounded-[7px] m-0.5">
+      <div className="w-full relative pb-0.5">
+        <h3 className="text-center text-[#fff] text-xl font-sequel-sans leading-tight tracking-tight mt-4">
+          System Health
+        </h3>
+        <div className="w-2/4 h-px absolute left-1/2 transform -translate-x-1/2 bg-[#fff]" />{" "}
+      </div>
+
+      <div className="flex flex-col items-center text-center s-full text-xs p-1">
+        <div className="bg-[#729987] w-full m-2 rounded-[7px] h-24 place-content-around w-full">
+          <h3 className="text-2xl">{systemHealth?.systemsLive}</h3>
+          <h3>Systems Live</h3>
+        </div>
+        <div className="bg-[#A65146] w-full rounded-[7px] h-24 place-content-around w-full">
+          <h3 className="text-2xl">{systemHealth?.systemsDown}</h3>
+          <h3>Systems Down</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default SystemHealthWidget;
--- smartessweb/frontend/src/app/dashboard/page.tsx
+++ smartessweb/frontend/src/app/dashboard/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import ProjectComponent from "../components/ProjectComponent";
+import DashboardWidget from "../components/DashboardComponents/DashboardWidget";
 
 const DashboardPage = () => {
   const router = useRouter();
@@ -16,12 +17,10 @@ const DashboardPage = () => {
 
   return (
     <div className="border border-black rounded-lg p-6 mx-4 lg:mx-8 mt-6 min-h-screen flex flex-col">
-      <div className="text-left text-[#325a67] text-[30px] leading-10 tracking-tight">
+      <div className="text-left text-[#325a67] text-[30px] leading-10 tracking-tight pb-4">
         Welcome to Your Dashboard
       </div>
-
-      {/* Widgets go here */}
-
+      <DashboardWidget />
       <div className=" pt-4 w-[306px] h-[66px] text-[#325a67] text-[30px]  leading-10 tracking-tight">
         Your Projects
       </div>
