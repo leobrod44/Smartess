0469aef8d8a72cb61d528b0e7975e7a64c27160c
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2024-10-31 13:06:37 -0400

    changing input type to remove defauly browser behavior #73

--- smartessweb/frontend/src/app/components/Searchbar.tsx
+++ smartessweb/frontend/src/app/components/Searchbar.tsx
@@ -25,7 +25,7 @@ export default function Searchbar({ onSearch }: SearchbarProps) {
             onChange={handleInputChange}
             className="block w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-10 text-[#14323b] placeholder:text-[#254752] ring-1 ring-inset ring-[#14323b] focus:outline-none focus:ring-[#14323b] sm:text-sm sm:leading-6"
             placeholder="Search"
-            type="search"
+            type="text"
           />
           <div className="absolute inset-y-0 right-2 flex items-center pointer-events-none">
             <Search className="h-5 w-5 text-[#14323b]" aria-hidden="true" />
