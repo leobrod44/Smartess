f5e45c891cb5922e12217bbbe7a26ce6b5a8aba4
TuanAnh-P <1tuananhp@gmail.com>
2024-11-01 17:49:28 -0400

    Fixed ESlint issue #130

--- smartessweb/frontend/src/app/components/DashboardNavbar.tsx
+++ smartessweb/frontend/src/app/components/DashboardNavbar.tsx
@@ -37,7 +37,7 @@ import Person2OutlinedIcon from '@mui/icons-material/Person2Outlined';
 import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
 import { usePathname, useRouter } from 'next/navigation';
 import AddressDropdown from './DashboardComponents/AddressDropdown';
-import { generateMockProjects, Project } from '../mockData';
+import { generateMockProjects } from '../mockData';
 
 // TypeScript Interface
 interface UserInfo {
@@ -697,7 +697,7 @@ const DashboardNavbar = () => {
                 >
                   <MenuButton className='-m-1.5 flex items-center p-1.5'>
                     <span className='sr-only'>Open user menu</span>
-                    <img
+                    <Image
                       className='h-8 w-8 rounded-full bg-gray-50'
                       src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
                       alt=''
