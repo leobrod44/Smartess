28ef2c237cf6a2464cfee91272210126f906144f
layanat <tounsilayana@gmail.com>
2024-11-02 19:40:49 -0400

    import User Info modal and Destruct addresses to show the full list #64 #132

--- smartessweb/frontend/src/app/components/ManageAccountsList.tsx
+++ smartessweb/frontend/src/app/components/ManageAccountsList.tsx
@@ -1,20 +1,32 @@
 "use client";
+import React, { useState } from "react";
 import EditIcon from "@mui/icons-material/Edit";
-
+import UserInfoModal from "../components/UserInfoModal";
 interface ManageAccountsListProps {
   address: string;
   userName: string;
   permission: "admin" | "basic" | "master";
   currentUserRole: "admin" | "basic" | "master"; // Current user's role
+  addresses: string[];
 }
 
-const ManageAccountsList: React.FunctionComponent<ManageAccountsListProps> = ({
+const ManageAccountsList = ({
   address,
   userName,
   permission,
   currentUserRole,
-}) => {
-  // Determine the background and text color based on the permission type
+  addresses, // Destructure addresses
+}: ManageAccountsListProps) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
+
   const getColorClasses = () => {
     switch (permission) {
       case "basic":
@@ -29,27 +41,39 @@ const ManageAccountsList: React.FunctionComponent<ManageAccountsListProps> = ({
   };
 
   return (
-    <div className="account-card border p-4 rounded shadow-sm flex items-center justify-between">
-      <div className="flex-1">
-        <p>{address}</p>
-      </div>
-      <div className="flex-1">
-        <p>{userName}</p>
-      </div>
-      <div className="flex-1">
-        <div
-          className={`w-[78px] h-8 px-5 rounded-[20px] flex items-center justify-center ${getColorClasses()}`}
-        >
-          <p className="text-sm font-medium">{permission}</p>
+    <>
+      <div className="account-card border p-4 rounded shadow-sm flex items-center justify-between">
+        <div className="flex-1">
+          <p>{address}</p>
         </div>
-      </div>
-      {/* Render the edit icon only if the current user has the "master" role */}
-      {(currentUserRole === "master" || currentUserRole === "admin") && (
-        <div className="ml-4">
-          <EditIcon className="text-[#30525E] cursor-pointer" />
+        <div className="flex-1">
+          <p>{userName}</p>
         </div>
-      )}
-    </div>
+        <div className="flex-1">
+          <div
+            className={`w-[78px] h-8 px-5 rounded-[20px] flex items-center justify-center ${getColorClasses()}`}
+          >
+            <p className="text-sm font-medium">{permission}</p>
+          </div>
+        </div>
+
+        {/* Render the edit icon only if the current user has the "master" or "admin" role */}
+        {(currentUserRole === "master" || currentUserRole === "admin") && (
+          <div className="ml-4" onClick={handleOpenModal}>
+            <EditIcon className="text-[#30525E] cursor-pointer" />
+          </div>
+        )}
+      </div>
+
+      {/* User Details Modal */}
+      <UserInfoModal
+        open={isModalOpen}
+        onClose={handleCloseModal}
+        userName={userName}
+        role={permission}
+        addresses={addresses} // Pass the full addresses array
+      />
+    </>
   );
 };
 
