df8f83fa5418be9bba8791bb0d48b484adcdbeeb
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2024-11-12 18:52:55 -0500

    US6.4 TASK-2 implementing UI of ticket info section (#204)
    * Bug Fix: added pagination for project component on dashboard #199
    
    * creating static UI of the ticket information section #174
    
    * added function to verify if the ticket text is short- if so, remove the see more button from view #174
    
    * Adding a max height to adjust truncation of ticket text #174
    
    * adjusting the padding bottom when toggling see less/ see more #174
    
    * Adding beginning of the css for the individual ticket page #174
    
    * Adjusting mock data-- adding ticket interface and generatemocktickets function #174
    
    * adding TEMPORARY button on ticket list page, to be able to access the individual ticket page, since right now we cannot access it directyl #174
    
    * adding reusable back arrow btn #174
    
    * adding the back arrow button component #174
    
    * adjusting date format on the individualticket component to fit month/day/year #174
    
    * Adjusting mockdata as discussed with abdullah, and adding fields for the unit number and project address in the mock data aswell. #174
    
    ---------
    
    Co-authored-by: layanat <tounsilayana@gmail.com>

---
+++ smartessweb/frontend/src/app/dashboard/individual-tickets/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import IndividualTicket from "@/app/components/IndividualTicket";
+import { generateMockProjects, Ticket } from "../../mockData";
+import BackArrowButton from "@/app/components/BackArrowBtn";
+
+const IndividualTicketPage = () => {
+  const [selectedTicket, setSelectedTicket] = useState<Ticket | null>(null);
+
+  useEffect(() => {
+    // Example to set the ticket (for demonstration, fetching from mock data)-- will be changed once backend is connected
+    const projects = generateMockProjects();
+    const specificTicket = projects[0]?.units[0]?.ticket[0];
+    if (specificTicket) {
+      setSelectedTicket(specificTicket);
+    }
+  }, []);
+
+  return (
+    <div>
+      <div className="border border-black rounded-lg p-6 mx-4 lg:mx-8 mt-6 min-h-screen flex flex-col">
+        <div className="flex items-center justify-between pb-4">
+          <div className="text-[#325a67] text-[30px] leading-10 tracking-tight">
+            Ticket Information
+          </div>
+          <BackArrowButton />
+        </div>
+        {selectedTicket && <IndividualTicket ticket={selectedTicket} />}
+      </div>
+    </div>
+  );
+};
+
+export default IndividualTicketPage;
