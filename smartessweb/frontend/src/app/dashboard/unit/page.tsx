7c6a5f0a42828ab83545b039e7ad843c470075f9
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2024-11-13 16:28:47 -0500

    U.S 6.1 T2: Filtering by project (#207)
    * Modified mock for filtering #206
    
    * Added filtering through navbar by project address #206

--- smartessweb/frontend/src/app/dashboard/unit/page.tsx
+++ smartessweb/frontend/src/app/dashboard/unit/page.tsx
@@ -8,7 +8,7 @@ import { generateMockProjects, Project } from "../../mockData";
 const UnitPage = () => {
   const [isMounted, setIsMounted] = useState(false);
   const [projects] = useState<Project[]>(generateMockProjects());
-  const { selectedProjectId, selectedProjectAddress } = useProjectContext();
+  const { selectedProjectAddress } = useProjectContext();
 
   useEffect(() => {
     setIsMounted(true);
@@ -18,24 +18,31 @@ const UnitPage = () => {
     return <p>Loading...</p>;
   }
 
+  // Mapping Units to components with Mock Data
   const allUnits = projects.flatMap((project) =>
     project.units.map((unit) => ({
       ...unit,
       projectAddress: project.address,
+      projectId: project.projectId,
+      hubUsers: project.hubUsers,
+      hubOwners: unit.owner,
+      pendingTickets: unit.ticket[2],
     }))
   );
 
+  // Filtering by project in Navbar
+  const filteredUnitsByProject = allUnits.filter((unit) => {
+    const effectiveProjectAddress = selectedProjectAddress || "ALL PROJECTS";
+    const matchesProjectAddress =
+      effectiveProjectAddress === "ALL PROJECTS"
+        ? true
+        : unit.projectAddress === effectiveProjectAddress;
+
+    return matchesProjectAddress;
+  });
+
   return (
     <div>
-      <h1>Unit Page</h1>
-      {selectedProjectId ? (
-        <>
-          <p>Selected Project ID: {selectedProjectId}</p>
-          <p>Project Address: {selectedProjectAddress}</p>
-        </>
-      ) : (
-        <p>No project selected.</p>
-      )}
       <div className="border border-black rounded-lg p-6 mx-4 lg:mx-8 mt-6">
         <div className="flex flex-row justify-between">
           <div className="flex flex-row">
@@ -48,7 +55,8 @@ const UnitPage = () => {
         </div>
 
         <div className="bg-[#4b7d8d] p-[10px] rounded-[7px] w-full mx-auto mt-4">
-          {allUnits.map((unit) => (
+          {/* Mapping of Filtered units by project selected in navar */}
+          {filteredUnitsByProject.map((unit) => (
             <UnitComponent
               key={unit.unitNumber}
               unit={unit}
