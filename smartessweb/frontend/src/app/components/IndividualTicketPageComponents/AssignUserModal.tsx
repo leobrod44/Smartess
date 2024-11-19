39f8cff64045e126753f84cdad2ae98a155a56d6
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2024-11-19 17:02:30 -0500

    WIP --> adding a set width to the modal to fix spacing. Adjusting props to include  list of available usrs for display to the modal #210

--- smartessweb/frontend/src/app/components/IndividualTicketPageComponents/AssignUserModal.tsx
+++ smartessweb/frontend/src/app/components/IndividualTicketPageComponents/AssignUserModal.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import CloseIcon from "@mui/icons-material/Close";
 import CondensedUserComponent from "./CondensedUserComponent";
+import { Individual } from "@/app/mockData";
 
-const AssignUserModalComponent = ({ onClose }: { onClose: () => void }) => {
+interface AssignUserModalProps {
+  onClose: () => void;
+  availableUsers: Individual[];
+  onAssignUser: (userId: string, isAssigned: boolean) => void;
+}
+
+const AssignUserModalComponent = ({
+  onClose,
+  availableUsers,
+  onAssignUser,
+}: AssignUserModalProps) => {
+  const handleToggleAssign = (userId: string, newState: boolean) => {
+    onAssignUser(userId, newState);
+  };
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="relative border-2 bg-white border-[#266472] rounded-md px-5 py-5 flex-col gap-3 inline-flex">
+      <div className=" w-[500px] relative border-2 bg-white border-[#266472] rounded-md px-5 py-5 flex-col gap-3 inline-flex">
         <CloseIcon
           className="absolute top-2 right-2 text-gray-400 cursor-pointer hover:text-gray-600 transition duration-300"
           onClick={onClose}
@@ -17,25 +31,28 @@ const AssignUserModalComponent = ({ onClose }: { onClose: () => void }) => {
         </div>
 
         <div className="w-full flex justify-between flex items-center text-[#266472]">
-          <div className="w-1/4 text-s leading-tight tracking-tight whitespace-nowrap">User ID</div>
-          <div className="w-1/2 text-s leading-tight tracking-tight whitespace-nowrap">Name</div>
-          <div className="w-1/4 text-s leading-tight tracking-tight whitespace-nowrap">Assign User</div>
+          <div className="w-1/4 text-s leading-tight tracking-tight whitespace-nowrap">
+            User ID
+          </div>
+          <div className="w-1/2 text-s leading-tight tracking-tight whitespace-nowrap">
+            Name
+          </div>
+          <div className="w-1/4 text-s leading-tight tracking-tight whitespace-nowrap">
+            Assign User
+          </div>
         </div>
 
-        <CondensedUserComponent
-          userId="123aws4"
-          firstName="Shirley"
-          lastName="Temple"
-          isAssigned={false}
-          onToggle={(newState) => console.log("Assigned:", newState)}
-        />
-        <CondensedUserComponent
-          userId="567asd8"
-          firstName="Megan"
-          lastName="Kelly Sitnky Winky Girly"
-          isAssigned={false}
-          onToggle={(newState) => console.log("Assigned:", newState)}
-        />
+        {availableUsers.map((user) => (
+          <CondensedUserComponent
+            key={user.individualId}
+            Individual={user}
+            isAssigned={false}
+            onToggle={(newState) =>
+              handleToggleAssign(user.individualId, newState)
+            }
+          />
+        ))}
+
         <div className="flex justify-center items-center mt-3 w-full">
           <button
             className="px-4 py-2 bg-[#266472] rounded-md text-center text-white text-s font-['Sequel Sans'] leading-tight tracking-tight hover:bg-[#14323b] transition duration-300"
