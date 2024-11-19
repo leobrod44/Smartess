3569f1abc6f62da326474e81744eac2603b3add1
Matthew Flaherty <matthewflaherty77@hotmail.com>
2024-11-19 17:09:33 -0500

    return all users in the same org AND projects

--- smartessweb/backend/controllers/manageAccountsController.js
+++ smartessweb/backend/controllers/manageAccountsController.js
@@ -28,14 +28,16 @@ exports.getOrgUsers = async (req, res) => {
 
         const { data: orgUserData, error: orgUserError } = await supabase
             .from('org_user')
-            .select('org_id')
+            .select('org_id, proj_id')
             .eq('user_id', userData.user_id);
 
         if (orgUserError) {
             return res.status(500).json({ error: 'Failed to fetch organization data.' });
         }
 
+        // get all organizations and projects for the current user
         const orgIds = orgUserData.map((org) => org.org_id);
+        const projIds = orgUserData.map((proj) => proj.proj_id);
 
         if (orgIds.length === 0) {
             return res.status(404).json({ error: 'No organizations found for this user.' });
@@ -44,7 +46,8 @@ exports.getOrgUsers = async (req, res) => {
         const { data: allOrgUsers, error: allOrgUsersError } = await supabase
             .from('org_user')
             .select('user_id, org_id, proj_id, org_user_type')
-            .in('org_id', orgIds);
+            .in('org_id', orgIds)
+            .in('proj_id', projIds);
 
         if (allOrgUsersError) {
             return res.status(500).json({ error: 'Failed to fetch organization users.' });
