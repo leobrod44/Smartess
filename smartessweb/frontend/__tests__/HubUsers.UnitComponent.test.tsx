3f62794f28d8f9f170e71ea1ecdc5ed20a2f87e9
Matthew Flaherty <55712024+mattflahertyy@users.noreply.github.com>
2024-11-19 00:19:00 -0500

    Us3.1 T4 -  Fetch org users (#221)
    * refactoring - changing User name to HubUser #216
    
    * more hub user refactor #216
    
    * added manage account routes #216 
    
    * route #216 
    
    * controller #216 
    
    * added new interface #216 
    
    * api #216
    
    * front end fetching methods #216
    
    * refactoring for hubusers #216
    
    * eslint #216
    
    * updating count names to avoid confusion #216 
    
    * cleanup #216 
    
    * eslint #216
    
    * fixing broken tests #216

--- smartessweb/frontend/__tests__/HubUsers.UnitComponent.test.tsx
+++ smartessweb/frontend/__tests__/HubUsers.UnitComponent.test.tsx
@@ -1,22 +1,23 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import HubUsers from "@/app/components/UnitComponents/HubUsers";
+import { HubUser } from "@/app/mockData";
 
 describe("HubUsers Component", () => {
-  const usersMock = [
-    { firstName: "Michael", lastName: "Johnson" },
-    { firstName: "Sarah", lastName: "Smith" },
-    { firstName: "David", lastName: "Williams" },
-    { firstName: "Alice", lastName: "Brown" },
+  const usersMock: HubUser[] = [
+    { tokenId: "1", firstName: "Michael", lastName: "Johnson", role: "basic" },
+    { tokenId: "2", firstName: "Sarah", lastName: "Smith", role: "admin" },
+    { tokenId: "3", firstName: "David", lastName: "Williams", role: "basic" },
+    { tokenId: "4", firstName: "Alice", lastName: "Brown", role: "admin" },
   ];
-
+  
   it("renders the hub users title correctly", () => {
-    render(<HubUsers users={usersMock} />);
+    render(<HubUsers hubUsers={usersMock} />);
     expect(screen.getByText("Hub users")).toBeInTheDocument();
   });
 
   it("displays the first three users with formatted names", () => {
-    render(<HubUsers users={usersMock} />);
+    render(<HubUsers hubUsers={usersMock} />);
 
     // Check if the formatted names for the first three users are displayed
     expect(screen.getByText("M. Johnson")).toBeInTheDocument();
@@ -25,21 +26,27 @@ describe("HubUsers Component", () => {
   });
 
   it("does not display more than three users", () => {
-    render(<HubUsers users={usersMock} />);
+    render(<HubUsers hubUsers={usersMock} />);
 
     // Check that the fourth user's name is not displayed
     expect(screen.queryByText("A. Brown")).not.toBeInTheDocument();
   });
 
   it("renders correctly when there are no users", () => {
-    render(<HubUsers users={[]} />);
+    render(<HubUsers hubUsers={[]} />);
 
     // Check if there are no user names displayed
     expect(screen.queryByText(/^[A-Z]\.\s[A-Za-z]+$/)).not.toBeInTheDocument();
   });
 
   it("formats names correctly to 'First Initial. Last Name'", () => {
-    render(<HubUsers users={[{ firstName: "Emily", lastName: "Clark" }]} />);
+    render(
+      <HubUsers
+        hubUsers={[
+          { tokenId: "5", firstName: "Emily", lastName: "Clark", role: "basic" },
+        ]}
+      />
+    );
 
     // Check formatted name
     expect(screen.getByText("E. Clark")).toBeInTheDocument();
