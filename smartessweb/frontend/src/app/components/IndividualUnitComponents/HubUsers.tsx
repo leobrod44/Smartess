92b2c6b374d095fa705592ece296b869933e46e0
layanat <tounsilayana@gmail.com>
2024-11-18 23:02:52 -0500

    fixed spacing #181

--- smartessweb/frontend/src/app/components/IndividualUnitComponents/HubUsers.tsx
+++ smartessweb/frontend/src/app/components/IndividualUnitComponents/HubUsers.tsx
@@ -12,19 +12,17 @@ const HubUsers = ({ hubUsers }: HubUsersProps) => {
         Hub Users
       </div>
 
-      {/* Separator Line */}
-      <div className="w-full h-px bg-[#4b7d8d] mb-4"></div>
-
       {/* Table Headers */}
-      <div className="grid grid-cols-4 w-full text-center text-[#325a67] font-semibold text-sm mb-2">
+      <div className="grid grid-cols-4 w-full text-center text-[#14323B] font-semibold text-sm mb-2">
         <div>User</div>
         <div>Telephone</div>
         <div>Email</div>
         <div>Contact</div>
       </div>
-
+      {/* Separator Line */}
+      <div className="w-full h-px bg-[#4b7d8d] mb-4"></div>
       {/* Table Rows */}
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-6">
         {hubUsers.map((user, index) => (
           <div
             key={index}
@@ -36,7 +34,7 @@ const HubUsers = ({ hubUsers }: HubUsersProps) => {
             <div>{user.telephone || "Not Provided"}</div>
             <div>{user.email}</div>
             <div>
-              <button className="w-[80px] h-[22px] bg-[#729987] rounded-md hover:bg-[#1f505e] transition duration-300 text-white text-xs font-medium">
+              <button className="w-[80px] h-[22px] ml-4 bg-[#729987] rounded-md hover:bg-[#1f505e] transition duration-300 text-white text-xs font-medium">
                 Contact
               </button>
             </div>
