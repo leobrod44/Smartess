3be52371a31a7315419c05df5b029832158dc219
Matthew Flaherty <matthewflaherty77@hotmail.com>
2024-11-22 13:27:33 -0500

    route #245

--- smartessweb/backend/routes/manageAccountsRoutes.js
+++ smartessweb/backend/routes/manageAccountsRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getCurrentUser, getOrgUsers, getOrgIndividualsData, getOrgUsersProjects, getOrgProjects } = require('../controllers/manageAccountsController');
+const { getCurrentUser, getOrgUsers, getOrgIndividualsData, getOrgUsersProjects, getOrgProjects, assignOrgUserToProject } = require('../controllers/manageAccountsController');
 const { verifyToken } = require('../middleware/middleware');
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.get('/get-org-users', verifyToken, getOrgUsers);
 router.post('/get-org-individuals-data', verifyToken, getOrgIndividualsData);
 router.post('/get-org-users-projects', verifyToken, getOrgUsersProjects);
 router.post('/get-org-projects', verifyToken, getOrgProjects);
+router.post('/assign-org-user-to-project', verifyToken, assignOrgUserToProject);
 
 module.exports = router;
