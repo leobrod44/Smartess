7d2bb90cdd6488b45e92c782c146ffb8731537fe
Matthew Flaherty <55712024+mattflahertyy@users.noreply.github.com>
2024-11-24 13:20:57 -0500

    US3.1 T9 - Remove org user from project (#253)
    * controller to remove project #247
    
    * route #247
    
    * api #247
    
    * front end functionality #247

--- smartessweb/backend/routes/manageAccountsRoutes.js
+++ smartessweb/backend/routes/manageAccountsRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getCurrentUser, getOrgUsers, getOrgIndividualsData, getOrgUsersProjects, getOrgProjects, assignOrgUserToProject } = require('../controllers/manageAccountsController');
+const { getCurrentUser, getOrgUsers, getOrgIndividualsData, getOrgUsersProjects, getOrgProjects, assignOrgUserToProject, removeOrgUserFromProject } = require('../controllers/manageAccountsController');
 const { verifyToken } = require('../middleware/middleware');
 const router = express.Router();
 
@@ -9,5 +9,6 @@ router.post('/get-org-individuals-data', verifyToken, getOrgIndividualsData);
 router.post('/get-org-users-projects', verifyToken, getOrgUsersProjects);
 router.post('/get-org-projects', verifyToken, getOrgProjects);
 router.post('/assign-org-user-to-project', verifyToken, assignOrgUserToProject);
+router.post('/remove-org-user-from-project', verifyToken, removeOrgUserFromProject);
 
 module.exports = router;
