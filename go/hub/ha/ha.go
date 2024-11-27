c970d3ea834b7061de64c1cded0af331243ef5dc
ChiefsBestPal <67126866+ChiefsBestPal@users.noreply.github.com>
2024-11-26 19:06:16 -0500

    Implemented utility and conversion methods for HA  ConciseEvent and ConciseAttributes

--- go/hub/ha/ha.go
+++ go/hub/ha/ha.go
@@ -2,6 +2,8 @@ package ha
 
 import (
 	"Smartess/go/common/structures"
+	"encoding/json"
+	"fmt"
 	"time"
 )
 
@@ -27,4 +29,38 @@ type ConciseAttributes struct {
 	FriendlyName      *string     `json:"friendly_name,omitempty"`
 	StateClass        *string     `json:"state_class,omitempty"`
 	SupportedFeatures interface{} `json:"supported_features,omitempty"`
+}
+
+func parseConciseEvent(rawJSON string) (*ConciseEvent, error) {
+	var event ConciseEvent
+	err := json.Unmarshal([]byte(rawJSON), &event)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing JSON: %v", err)
+	}
+	return &event, nil
+}
+func ConvertEventToConciseEvent(event *structures.State) *ConciseEvent {
+	return &ConciseEvent{
+		Attributes:  *ConvertAttributesToConciseAttributes(event.Attributes),
+		Context:     event.Context,
+		EntityID:    event.EntityID,
+		LastChanged: event.LastChanged,
+		LastUpdated: event.LastUpdated,
+		State:       event.State,
+	}
+}
+func ConvertAttributesToConciseAttributes(attributes map[string]interface{}) *ConciseAttributes {
+
+	return &ConciseAttributes{
+		DeviceClass:       getStringPtr(attributes, "device_class"),
+		FriendlyName:      getStringPtr(attributes, "friendly_name"),
+		StateClass:        getStringPtr(attributes, "state_class"),
+		SupportedFeatures: attributes["supported_features"],
+	}
+}
+func getStringPtr(attributes map[string]interface{}, key string) *string {
+	if value, ok := attributes[key].(string); ok {
+		return &value
+	}
+	return nil
 }
