888488e9532e4cc641168797c531d35551691712
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2024-11-28 11:29:34 -0500

    allowing ticket to be closed without anyone assigned #262

--- smartessweb/frontend/src/app/components/IndividualTicketPageComponents/ManageTicketAssignment.tsx
+++ smartessweb/frontend/src/app/components/IndividualTicketPageComponents/ManageTicketAssignment.tsx
@@ -59,7 +59,7 @@ function ManageTicketAssignment({ ticket, onStatusUpdate }: ManageTicketProps) {
 
   useEffect(() => {
     // Check if there are no assigned users, and if the status isnt open. if so, change it to open
-    if (assignedUsers.length === 0 && ticket.status !== "open") {
+    if (assignedUsers.length === 0 && ticket.status !== "closed") {
       onStatusUpdate("open");
     }
   }, [assignedUsers, onStatusUpdate, ticket.status]);
@@ -107,7 +107,7 @@ function ManageTicketAssignment({ ticket, onStatusUpdate }: ManageTicketProps) {
     }
   };
 
-  //assigning the current user to the ticket 
+  //assigning the current user to the ticket
   const assignYourself = () => {
     try {
       //convert crrentUser to an individual..... have to do thisbecause current user
@@ -122,7 +122,7 @@ function ManageTicketAssignment({ ticket, onStatusUpdate }: ManageTicketProps) {
 
       // Convert currentUser to Individual type
       const currentUserAsIndividual: Individual = {
-        individualId: parseInt(currentUser.userId, 10), 
+        individualId: parseInt(currentUser.userId, 10),
         firstName: currentUser.firstName,
         lastName: currentUser.lastName,
         role: currentUser.role,
@@ -174,31 +174,35 @@ function ManageTicketAssignment({ ticket, onStatusUpdate }: ManageTicketProps) {
   return (
     <>
       {ticket.status === "closed" ? ( //if ticket is closed, display this section with the previously assigned users
-        <div className="w-full px-2.5 bg-white rounded-[38px] shadow border-2 border-[#254752]/30 shadow-xl pb-6">
-          <div className="text-[#254752] text-[20px] font-sequel-sans w-full  pt-6 flex items-center justify-between">
-            Users Previously Assigned to this ticket
-          </div>
+        <div className="w-full px-2 bg-white rounded-[38px] pb-6">
+          {ticket.assigned_employees.length !== 0 ? (
+            <>
+              <div className="text-[#254752] px-3 text-[20px] font-sequel-sans w-full  flex items-center justify-between">
+                Users Previously Assigned To This Ticket
+              </div>
+              <div className="w-full px-3 mt-6 flex justify-between text-[#266472] text-s font-sequel-sans">
+                <div className="flex-1 text-left">ID</div>
+                <div className="flex-1 text-left">Name</div>
+                <div className="flex-1 text-center"> Contact </div>
+                <div className="flex-1 text-right mr-3">Resolved</div>
+              </div>
 
-          <div className="w-full px-3 mt-6 flex justify-between text-[#266472] text-s font-sequel-sans">
-            <div className="flex-1 text-left">ID</div>
-            <div className="flex-1 text-left">Name</div>
-            <div className="flex-1 text-center"> Contact </div>
-            <div className="flex-1 text-right mr-3">Resolved</div>
-          </div>
-
-          {ticket.assigned_employees.map((Individual, index) => (
-            <AssignedUserClosedTicket key={index} Individual={Individual} />
-          ))}
+              {ticket.assigned_employees.map((Individual, index) => (
+                <AssignedUserClosedTicket key={index} Individual={Individual} />
+              ))}
+            </>
+          ) : (
+            <>
+              <div className="text-[#266472] px-3 text-[18px font-sequel-sans w-full">
+                No Users Were Assigned To This Ticket When Closed
+              </div>
+            </>
+          )}
         </div>
       ) : assignedUsers.length === 0 ? ( //if ticket has no users assigned, display section to assign users
         <>
-          <div className="w-full px-2.5 bg-white rounded-[38px] shadow border-2 border-[#254752]/30 shadow-xl pb-6">
-            <div className="w-full px-[13px] pt-6 flex items-center justify-between">
-              <div className="text-[#254752] text-[20px] font-sequel-sans">
-                Users Assigned to this ticket
-              </div>
-            </div>
-            <div className=" px-[13px] text-[#254752] text-s font-['Sequel Sans'] mt-6">
+          <div className="w-full px-2.5 bg-white rounded-[38px] pb-6">
+            <div className=" px-3 text-[#254752] text-s font-['Sequel Sans'] ">
               There are currently no users assigned to this ticket.{" "}
               <span
                 className="text-[#266472] underline hover:text-[#254752] cursor-pointer transition duration-300"
@@ -220,9 +224,9 @@ function ManageTicketAssignment({ ticket, onStatusUpdate }: ManageTicketProps) {
       ) : (
         //if ticket has some users assigned, display them
         <>
-          <div className="w-full px-2.5 bg-white rounded-[38px] shadow border-2 border-[#254752]/30 shadow-xl pb-6">
+          <div className="w-full px-2.5 bg-white rounded-[38px] shadow border-2 border-[#254752]/30 shadow-xl pb-6 mb-10">
             <div className="text-[#254752] text-[20px] font-sequel-sans w-full px-[13px] pt-6 flex items-center justify-between">
-              Users Assigned to this ticket
+              Users Assigned To This Ticket
             </div>
 
             <div className="w-full px-3 mt-6 flex justify-between text-[#266472] text-s font-sequel-sans">
