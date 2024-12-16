a8b11bff4bf4559ec406af2a0d5d039181786d48
Abdullah A <97749196+AA789-ai@users.noreply.github.com>
2024-12-16 17:38:21 -0500

    Separated containers for smartessweb frontend and backend which will make it easier to deploy.

--- smartessweb/frontend/src/api/api.ts
+++ smartessweb/frontend/src/api/api.ts
@@ -1,1 +1,3 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api';
+export const API_URL = process.env.NODE_ENV === 'production' 
+  ? 'http://localhost:3001/api'
+  : 'http://localhost:3000/api';
