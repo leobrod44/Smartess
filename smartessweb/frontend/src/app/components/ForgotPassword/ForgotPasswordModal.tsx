c17b6b2d678401a0e588ac5bc686ac89339aa270
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-01-06 15:45:47 -0500

    Changing cancel and reset buttons to ensure uniformity across buttons #161

--- smartessweb/frontend/src/app/components/ForgotPassword/ForgotPasswordModal.tsx
+++ smartessweb/frontend/src/app/components/ForgotPassword/ForgotPasswordModal.tsx
@@ -81,13 +81,13 @@ const ForgotPasswordModal = ({
 
         <div className="flex justify-around">
           <button
-            className="bg-[#9b211b] text-white text-xs w-[110px] py-2 rounded-md transition duration-300"
+            className="bg-[#ff5449] text-white text-xs w-[110px] py-2 rounded-md hover:bg-[#9b211b] transition duration-300"
             onClick={onClose}
           >
             Cancel
           </button>
           <button
-            className="bg-[#254752] text-white text-xs w-[110px] py-2 rounded-md transition duration-300"
+            className="bg-[#4b7d8d] text-white text-xs w-[110px] py-2 rounded-md hover:bg-[#254752] transition duration-300"
             onClick={handleReset}
           >
             Reset
