2ab039908dbf32549edffa9e981582662246795b
Leo Brodeur <65002959+leobrod44@users.noreply.github.com>
2025-01-07 19:22:12 -0500

    HUB1-T1--process rtsp streams (#293)
    * rtsp added
    
    * Update docker-compose.yaml

---
+++ go/hub/hub_daemon.go
@@ -0,0 +1,79 @@
+package hub
+
+import (
+	common_rabbitmq "Smartess/go/common/rabbitmq"
+	structures "Smartess/go/common/structures"
+	"Smartess/go/hub/events"
+	logs "Smartess/go/hub/logger"
+	"Smartess/go/hub/rtsp"
+	"errors"
+	"fmt"
+	"sync"
+)
+
+type SmartessHub struct {
+	instance       *common_rabbitmq.RabbitMQInstance
+	event_handler  *events.EventHandler
+	rtsp_processor *rtsp.RtspProcessor
+	Logger         *logs.Logger
+}
+
+func Init(selectedHub structures.HubTypeEnum) (SmartessHub, error) {
+	logger, err := logs.NewRabbitMQLogger()
+	if err != nil {
+		return SmartessHub{}, errors.New("Failed to initialize RabbitMQ logger: " + err.Error())
+	}
+
+	instance, err := common_rabbitmq.Init("/app/config/queues.yaml") //todo test with exchanges.yaml vs queues.yaml common/config files... merge or not ?
+	if err != nil {
+		return SmartessHub{}, errors.New("Failed to initialize RabbitMQ instance: " + err.Error())
+	}
+	event_handler, err := events.Init(selectedHub, logger, instance)
+	logger.Info("Event Handler initialized")
+	if err != nil {
+		return SmartessHub{}, errors.New("Failed to initialize EventHandler: " + err.Error())
+	}
+
+	// Declare the topic exchange
+	err = instance.Channel.ExchangeDeclare(
+		common_rabbitmq.Test0TopicExchangeName, // name of the exchange
+		"topic",                                // type of exchange
+		true,                                   // durable
+		false,                                  // auto-deleted
+		false,                                  // internal
+		false,                                  // no-wait
+		nil,                                    // arguments
+	)
+	if err != nil {
+		return SmartessHub{}, fmt.Errorf("Failed to declare topic exchange: %v", err)
+	}
+	logger.Info("Declared topic exchange")
+	rtsp_processor, err := rtsp.Init(instance, logger)
+	if err != nil {
+		return SmartessHub{}, errors.New("Failed to initialize RTSP Processor: " + err.Error())
+	}
+	logger.Info("RTSP Processor initialized")
+
+	return SmartessHub{
+		instance:       instance,
+		event_handler:  &event_handler,
+		rtsp_processor: &rtsp_processor,
+		Logger:         logger,
+	}, nil
+}
+
+func (hub *SmartessHub) Start(selectedHub structures.HubTypeEnum) {
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		defer wg.Done() // Decrement the counter when this goroutine completes
+		hub.event_handler.Start(selectedHub)
+	}()
+	go func() {
+		defer wg.Done() // Decrement the counter when this goroutine completes
+		hub.rtsp_processor.Start()
+	}()
+	wg.Wait()
+	hub.rtsp_processor.Close()
+	hub.event_handler.Close()
+}
