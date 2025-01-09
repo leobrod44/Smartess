a4592ac841a92b719badadf3930342c448dd9bde
ChiefsBestPal <67126866+ChiefsBestPal@users.noreply.github.com>
2025-01-09 01:25:05 -0500

    more testing for go ci

--- go/testci.go
+++ go/testci.go
@@ -1,24 +1,85 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"os"
+	"github.com/streadway/amqp"
+	"log"
 )
 
+type Person struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
 func main() {
-	fmt.Println("Testing CI setup")
-
 	// Unused variable
-	unusedVar := 42
+	var unusedVar int
 
-	// Inefficient assignment
-	var x int
-	x = 1
+	// Incorrect JSON handling
+	jsonStr := `{"name": "John", "age": "thirty"}`
+	var person Person
+	err := json.Unmarshal([]byte(jsonStr), &person)
+	if err != nil {
+		log.Println("Error unmarshalling JSON:", err)
+	}
 
-	// Error not checked
-	file, _ := os.Open("nonexistentfile.txt")
-	defer file.Close()
+	// Incorrect error handling
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		log.Println("Failed to connect to RabbitMQ")
+	}
+	defer conn.Close()
 
-	// Formatting issue
-	fmt.Println("This line has a formatting issue")
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Println("Failed to open a channel")
+	}
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"test_queue",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		log.Println("Failed to declare a queue")
+	}
+
+	body := "Hello World!"
+	err = ch.Publish(
+		"",
+		q.Name,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	if err != nil {
+		log.Println("Failed to publish a message")
+	}
+
+	// Incorrect variable naming
+	var json_data string = `{"name": "Alice", "age": 25}`
+	var p Person
+	json.Unmarshal([]byte(json_data), &p)
+	fmt.Println(p)
+
+	// Inefficient string concatenation
+	greeting := "Hello, " + p.Name + "!"
+
+	// Unused import
+	_ = fmt.Sprintf("This is a test: %s", greeting)
+
+	// Incorrect defer usage
+	defer fmt.Println("This will not run if there's an error above")
+
+	// Incorrect error handling
+	if err != nil {
+		fmt.Println("An error occurred")
+	}
 }
