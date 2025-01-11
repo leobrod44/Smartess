7f642482969a1ac19d18cc7ca29a5e4dc4a0eb79
SenecalRenaud <senecalrenaud@gmail.com>
2025-01-11 14:40:41 -0500

    Added alert consumption from rabbit queues in website backend

--- smartessweb/backend/server.js
+++ smartessweb/backend/server.js
@@ -3,6 +3,22 @@ require('dotenv').config();
 const app = require('./app');
 const port = process.env.PORT || 3001;
 
+const RabbitMQService = require('./rabbitmq-consumers/defaultConsumer.js'); 
+const rabbitMQService = new RabbitMQService();
+
+async function startRabbitMQService() {
+  try {
+    await rabbitMQService.init();
+    await rabbitMQService.setupExchangesAndQueues();
+    await rabbitMQService.startConsuming();
+    console.log('RabbitMQ Service started successfully');
+  } catch (err) {
+    console.error('Failed to start RabbitMQ Service', err);
+  }
+}
+
+startRabbitMQService();
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
