f50206a5f256a5954801ec40b9246249e50c9ddd
Matthew Flaherty <matthewflaherty77@hotmail.com>
2025-01-12 17:56:01 -0500

    new route #303

--- smartessweb/backend/routes/individualUnitRoutes.js
+++ smartessweb/backend/routes/individualUnitRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { getIndividualUnit} = require('../controllers/individualUnitContoller');
+const { getIndividualUnit, removeUserFromHub} = require('../controllers/individualUnitContoller');
 const { verifyToken } = require('../middleware/middleware');
 const router = express.Router();
 
 router.post('/get-individual-unit', verifyToken, getIndividualUnit);
+router.post('/remove-user-from-hub', verifyToken, removeUserFromHub);
 
 module.exports = router;
