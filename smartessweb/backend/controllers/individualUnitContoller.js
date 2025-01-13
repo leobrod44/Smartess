b2ccce39935f3ddcb5ac919b661674e0fe6a81c1
Matthew Flaherty <matthewflaherty77@hotmail.com>
2025-01-12 21:29:27 -0500

    controller for fetching current user #303

--- smartessweb/backend/controllers/individualUnitContoller.js
+++ smartessweb/backend/controllers/individualUnitContoller.js
@@ -1,5 +1,69 @@
 const supabase = require('../config/supabase');
 
+exports.getCurrentUser = async (req, res) => {
+    try {
+        const token = req.token;
+        
+        const { data: { user }, error: authError } = await supabase.auth.getUser(token);
+        
+        if (authError) {
+            return res.status(401).json({ error: 'Invalid token' });
+        }
+
+        const { data: userData, error: userError } = await supabase
+        .from('user')
+        .select('user_id')
+        .eq('email', user.email)
+        .single();
+
+        if (userError) {
+            return res.status(500).json({ error: 'Failed to fetch user data.' });
+        }
+
+        if (!userData) {
+            return res.status(404).json({ error: 'User not found.' });
+        }
+
+        const { data: currentUser, error: currentUserError } = await supabase
+        .from('org_user')
+        .select('user_id, proj_id, org_id, org_user_type')
+        .eq('user_id', userData.user_id);
+
+        if (currentUserError) {
+            return res.status(500).json({ error: 'Failed to fetch current user data.' });
+        }
+
+        if (!currentUser) {
+            return res.status(404).json({ error: 'Current user not found.' });
+        }
+
+        const projectIds = currentUser.map((cUser) => cUser.proj_id);
+        const { data: projects, error: projectError } = await supabase
+            .from('project')
+            .select('address')
+            .in('proj_id', projectIds);
+
+        if (projectError) {
+            return res.status(500).json({ error: 'Failed to fetch project addresses.' });
+        }
+
+        const addresses = projects.map((project) => project.address);
+        const role = currentUser[0].org_user_type; 
+
+        const formattedCurrentUser = {
+            userId: userData.user_id,
+            role: role === 'master' || role === 'admin' || role === 'basic' ? role : 'basic', 
+            address: addresses
+        };
+
+        res.json({ currentUser: formattedCurrentUser });
+        
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal server error.' });
+    }
+}
+
 exports.getIndividualUnit = async (req, res) => {
     try {                
         const token = req.token;
@@ -45,7 +109,7 @@ exports.getIndividualUnit = async (req, res) => {
         if (ownerData) {
             const { data: ownerUserData, error: ownerError } = await supabase
                 .from('user')
-                .select('user_id, first_name, last_name, email')
+                .select('user_id, first_name, last_name, email, phone_number')
                 .eq('user_id', ownerData.user_id)
                 .single();
 
@@ -55,6 +119,7 @@ exports.getIndividualUnit = async (req, res) => {
                     firstName: ownerUserData.first_name,
                     lastName: ownerUserData.last_name,
                     email: ownerUserData.email,
+                    telephone: ownerUserData.phone_number
                 };
             }
         }
@@ -92,7 +157,7 @@ exports.getIndividualUnit = async (req, res) => {
         const hubUsers = await Promise.all(filteredHubUsersData.map(async (hubUser) => {
             const { data: userData, error: userError } = await supabase
                 .from('user')
-                .select('user_id, first_name, last_name, email')
+                .select('user_id, first_name, last_name, email, phone_number')
                 .eq('user_id', hubUser.user_id)
                 .single();
 
@@ -105,6 +170,7 @@ exports.getIndividualUnit = async (req, res) => {
                 firstName: userData.first_name,
                 lastName: userData.last_name,
                 email: userData.email,
+                telephone: userData.phone_number
             };
         }));
 
