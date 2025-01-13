04a3508c15dc6386f9956279b5b20fed42c12b1e
Matthew Flaherty <matthewflaherty77@hotmail.com>
2025-01-12 21:29:37 -0500

    route for fetching current user #303

--- smartessweb/backend/routes/individualUnitRoutes.js
+++ smartessweb/backend/routes/individualUnitRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { getIndividualUnit, removeUserFromHub} = require('../controllers/individualUnitContoller');
+const { getCurrentUser, getIndividualUnit, removeUserFromHub} = require('../controllers/individualUnitContoller');
 const { verifyToken } = require('../middleware/middleware');
 const router = express.Router();
 
+router.get('/get-current-user', verifyToken, getCurrentUser);
 router.post('/get-individual-unit', verifyToken, getIndividualUnit);
 router.post('/remove-user-from-hub', verifyToken, removeUserFromHub);
 
