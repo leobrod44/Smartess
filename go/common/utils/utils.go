00d6fa37f353d32274a8b72815cd5f598ac89a93
Antoine <67126866+ChiefsBestPal@users.noreply.github.com>
2025-01-18 20:18:20 -0500

    "ConciseEvents"  Smart HA parsing to MQ keys: Home Assistant structs, events,  (#346)
    * Created specific structs for reading concisely HA events
    
    * Implemented utility and conversion methods for HA  ConciseEvent and ConciseAttributes
    
    * Made some useful utils and reduced repetitve overhead in ha.go and others.
    
    * Implemented a System for event classification that summarizes with tags and a Class name possibly very complex multi-field HA events
    
    * Formated a json of 58 HA raw events to be used for realistic mock or tests in ./go/hub/ha modules and utilities
    
    * Added some relevant comments for HA in different files
    
    * Implemented 2 utils: Authed Get http request and string normalization
    
    * Added a few HomeAssistant related structs (documented)
    
    * Made template Zigbee (documented) devices/events and trivial mq key routing generation
    
    * Created HomeAssistantInstance (hass: api, registries, bus, config)

---
+++ go/common/utils/utils.go
@@ -0,0 +1,69 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"regexp"
+	"strings"
+)
+
+// Parses simply a raw JSON string into a generic T type struct (Warning: generic T requires newer go version, else interface{})
+func ParseJsonBasic[T any](rawJSON string) (*T, error) {
+	var result T
+	err := json.Unmarshal([]byte(rawJSON), &result)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing JSON: %v", err)
+	}
+	return &result, nil
+}
+
+// Fetches a string pointer from a map of fields, if the key exists and the value is a string else nil
+func GetStringPtrNilSafe(fields map[string]interface{}, key string) *string {
+	if value, ok := fields[key].(string); ok {
+		return &value
+	}
+	return nil
+}
+func NormalizeStringToTag(rawTag string) string {
+	normalized := strings.ToLower(strings.ReplaceAll(rawTag, " ", "_"))
+	reg := regexp.MustCompile(`[^a-z0-9_]+`)
+	return reg.ReplaceAllString(normalized, "")
+}
+
+// Generic function to make API calls and unmarshal the response into a given structure type
+func GetDataFromAPIendpoint(fullApiUrl, token string, result interface{}) error {
+
+	req, err := http.NewRequest("GET", fullApiUrl, nil)
+	if err != nil {
+		return fmt.Errorf("error creating request: %v", err)
+	}
+
+	// Set the Authorization header for JWT-like or long-lived tokens
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("error making HTTP request: %v", err)
+	}
+	//nolint:errcheck
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("error: expected 200 OK, got %v", resp.Status)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("error reading response body: %v", err)
+	}
+	if err := json.Unmarshal(body, result); err != nil {
+		if err := json.NewDecoder(resp.Body).Decode(result); err != nil {
+			return fmt.Errorf("error unmarshaling / decoding response: %v", err)
+		}
+	}
+
+	return nil
+}
