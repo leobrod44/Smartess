18a4c342dc345bf6c9ec7cd389f0cddc66d91687
Abdullah A <97749196+AA789-ai@users.noreply.github.com>
2025-01-20 20:24:20 -0500

    US 6.3 T4: Allow admin and Master to delete tickets (#373)
    * created the backend logic for tickets
    
    * Set up frontend to be able to delete tickets db
    
    ---------
    
    Co-authored-by: TuanAnh-P <1tuananhp@gmail.com>

--- smartessweb/backend/routes/ticketsRoutes.js
+++ smartessweb/backend/routes/ticketsRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { getTickets } = require('../controllers/ticketsController');
+const { getTickets, deleteTicket } = require('../controllers/ticketsController');
 const { verifyToken } = require('../middleware/middleware');
 const router = express.Router();
 
 router.get('/get-tickets', verifyToken, getTickets);
+router.delete('/delete-ticket/:ticket_id', verifyToken, deleteTicket);
 
 module.exports = router;
