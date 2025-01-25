5ec14f5a0bc51003f87d5bdf2b700a73c791f998
Abdullah A <97749196+AA789-ai@users.noreply.github.com>
2025-01-25 12:36:27 -0500

    Added the individual fetching page. It fetches the specific ticket, the user who submits it and the property's address. (#429)
    #428

---
+++ smartessweb/frontend/src/api/dashboard/ticket/[ticketid]/page.ts
@@ -0,0 +1,47 @@
+interface APITicket {
+    ticket_id: string;
+    proj_id: string;
+    unit_id: string;
+    name: string;
+    description: string;
+    type: string;
+    unit: string;
+    status: string;
+    created_at: string;
+    submitted_by_firstName: string;
+    submitted_by_lastName: string;
+    submitted_by_email: string;
+    project_address: string;
+   }
+   
+   const getIndividualTicket = async (token: string, ticketId: string): Promise<{ticket: APITicket}> => {
+    const response = await fetch(`http://localhost:3000/api/tickets/ticket/${ticketId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+   
+    if (!response.ok) {
+      throw new Error('Failed to fetch ticket');
+    }
+   
+    return response.json();
+   };
+   
+   const deleteTicket = async (token: string, ticketId: string): Promise<void> => {
+    const response = await fetch(`http://localhost:3000/api/tickets/delete-ticket/${ticketId}`, {
+      method: 'DELETE',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+   
+    if (!response.ok) {
+      throw new Error('Failed to delete ticket');
+    }
+   };
+   
+   export const ticketsApi = {
+    getIndividualTicket,
+    deleteTicket,
+   };
