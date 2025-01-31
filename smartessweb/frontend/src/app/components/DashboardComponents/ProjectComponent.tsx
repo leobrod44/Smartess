f6263758e6b166a062f11f3c9583e3da72c4c161
layanat <tounsilayana@gmail.com>
2025-01-31 15:00:54 -0500

    changed spacing of dropdown button  #438

--- smartessweb/frontend/src/app/components/DashboardComponents/ProjectComponent.tsx
+++ smartessweb/frontend/src/app/components/DashboardComponents/ProjectComponent.tsx
@@ -104,7 +104,7 @@ export default function ProjectInfo({ projects }: ProjectInfoProps) {
               <div className="flex justify-center">
                 <button
                   aria-label="View Units"
-                  className="bg-[#4b7d8d]  text-white w-[50px] h-10 rounded-lg shadow-md flex items-center justify-center  hover:bg-[#266472] hover:scale-105 transition-transform duration-300"
+                  className="bg-[#4b7d8d]  text-white w-[78px] h-8 rounded-lg shadow-md flex items-center justify-center  hover:bg-[#266472] hover:scale-105 transition-transform duration-300"
                   onClick={() => handleToggle(startIndex + index)}
                 >
                   {showUnits === startIndex + index ? (
