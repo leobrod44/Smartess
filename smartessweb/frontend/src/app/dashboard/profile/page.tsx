8f4ddf223c212833c4529b31cd1b3923dfe2e878
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-02-04 11:15:50 -0500

    US 3.8 T3 Static UI of the profile page (#466)
    * Components for static UI of profile page #243
    
    * Fixing card styling and allignment #243
    
    * Fixing the styling on the profileCard.tsx
    
    * Using mock data to map currentUser to ProfilePage #243

--- smartessweb/frontend/src/app/dashboard/profile/page.tsx
+++ smartessweb/frontend/src/app/dashboard/profile/page.tsx
@@ -1,7 +1,38 @@
+import ProfileCard from "@/app/components/ProfileComponents/ProfileCard";
+import ProfileInfo from "@/app/components/ProfileComponents/ProfileInfo";
+
+/*
+  Current user is used for testing of Static UI
+*/
+
+export const currentUser = {
+  userId: "12345",
+  role: "master",
+  address: ["123 Main St", "Suite 400"],
+  firstName: "Admin",
+  lastName: "Admina",
+  email: "Admina@cs.smartess.ca",
+  phoneNumber: "514-444-1234",
+};
+
 const ProfilePage = () => {
   return (
-    <div>
-      <h1>Profile Page</h1>
+    <div className="border border-black rounded-lg p-6 mx-4 lg:mx-8">
+      <div className="flex flex-col justify-between">
+        <div className="flex flex-row ">
+          <h2 className="text-left text-[#325a67] text-[30px] leading-10 tracking-tight pb-3">
+            <h4>Profile</h4>
+          </h2>
+        </div>
+
+        <div className="flex flex-col sm:flex-row justify-normal gap-4">
+          {/* Right panel */}
+          <ProfileCard currentUser={currentUser} />
+
+          {/* Left panel */}
+          <ProfileInfo currentUser={currentUser} />
+        </div>
+      </div>
     </div>
   );
 };
