6f475dd4c4c61ac47c711dcd0917707e697354f9
layanat <tounsilayana@gmail.com>
2025-02-04 14:10:45 -0500

    created layout of alert page and column labels #239

--- smartessweb/frontend/src/app/dashboard/alerts/page.tsx
+++ smartessweb/frontend/src/app/dashboard/alerts/page.tsx
@@ -1,9 +1,35 @@
-const LockPage = () => {
+"use client";
+
+const AlertPage = () => {
   return (
-    <div>
-      <h1>Alerts Page</h1>
+    <div className="border border-black rounded-lg p-6 mx-4 lg:mx-8 mt-6 min-h-screen flex flex-col">
+      <div className="flex items-center pt-4 justify-between mb-8">
+        <div className="w-full text-[#325a67] text-[30px] leading-10 tracking-tight whitespace-nowrap">
+          Alerts
+        </div>
+      </div>
+      <div className="flex font-semibold border-b-2 border-black pb-2 mb-4">
+        <p className="flex-1 pl-2 text-[#30525E] text-lg font-sequel-sans-medium leading-tight tracking-tight">
+          Id
+        </p>
+        <p className="flex-1 pl-2  text-[#30525E] text-lg font-sequel-sans-medium leading-tight tracking-tight">
+          Alert
+        </p>
+        <p className="flex-1 pr-6 text-[#30525E] text-lg font-sequel-sans-medium leading-tight tracking-tight">
+          Type
+        </p>
+        <p className="flex-1 pr-6 text-[#30525E] text-lg font-sequel-sans-medium leading-tight tracking-tight">
+          Date
+        </p>
+        <p className="flex-1 pr-6 text-[#30525E] text-lg font-sequel-sans-medium leading-tight tracking-tight">
+          Time
+        </p>
+        <p className="flex-1 pr-6 text-[#30525E] text-lg font-sequel-sans-medium leading-tight tracking-tight">
+          Action
+        </p>
+      </div>
     </div>
   );
 };
 
-export default LockPage;
+export default AlertPage;
