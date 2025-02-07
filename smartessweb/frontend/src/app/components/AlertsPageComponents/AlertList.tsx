1e815afdd9aac922e8dd209877b95ac837e67e58
layanat <tounsilayana@gmail.com>
2025-02-07 15:16:06 -0500

    used "type" data instead of  "message data" to render icons

--- smartessweb/frontend/src/app/components/AlertsPageComponents/AlertList.tsx
+++ smartessweb/frontend/src/app/components/AlertsPageComponents/AlertList.tsx
@@ -8,8 +8,13 @@ import WaterDamageIcon from "@mui/icons-material/WaterDamage";
 import DeviceThermostatIcon from "@mui/icons-material/DeviceThermostat";
 import BatteryAlertIcon from "@mui/icons-material/BatteryAlert";
 import DangerousIcon from "@mui/icons-material/Dangerous";
-import WindowIcon from "@mui/icons-material/Window";
+import AcUnitIcon from "@mui/icons-material/AcUnit";
 import DoorBackIcon from "@mui/icons-material/DoorBack";
+import DirectionsRunIcon from "@mui/icons-material/DirectionsRun";
+import SensorsIcon from "@mui/icons-material/Sensors";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import WbIncandescentIcon from "@mui/icons-material/WbIncandescent";
+
 import { TrashIcon } from "@heroicons/react/24/outline";
 
 export interface AlertListProps {
@@ -17,26 +22,35 @@ export interface AlertListProps {
 }
 
 // Map alert messages to corresponding icons
-const getAlertIcon = (message: string) => {
-  switch (message) {
-    case "Smoke Alarm Activated":
+const getAlertIcon = (type: string) => {
+  switch (type) {
+    case "Smoke":
       return (
         <LocalFireDepartmentIcon className="text-red-500" fontSize="large" />
       );
-    case "Water Leak Detected":
+    case "Water":
       return <WaterDamageIcon className="text-blue-500" fontSize="large" />;
-    case "Thermostat > 25°C":
+    case "Temperature":
       return (
         <DeviceThermostatIcon className="text-orange-500" fontSize="large" />
       );
-    case "No Battery In Device":
+
+    case "BatteryLow":
       return <BatteryAlertIcon className="text-gray-500" fontSize="large" />;
-    case "Carbon Monoxide Detected":
-      return <DangerousIcon className="text-red-600" fontSize="large" />;
-    case "Window Opened":
-      return <WindowIcon className="text-green-500" fontSize="large" />;
-    case "Door Unlocked":
+    case "Motion":
+      return <DirectionsRunIcon className="text-red-600" fontSize="large" />;
+    case "DoorOpen":
       return <DoorBackIcon className="text-orange-500" fontSize="large" />;
+    case "Sensor":
+      return <SensorsIcon className="text-orange-500" fontSize="large" />;
+    case "Climate":
+      return <AcUnitIcon className="text-orange-500" fontSize="large" />;
+    case "Unknown":
+      return <HelpOutlineIcon className="text-orange-500" fontSize="large" />;
+    case "Light":
+      return (
+        <WbIncandescentIcon className="text-orange-500" fontSize="large" />
+      );
     default:
       return <DangerousIcon className="text-gray-400" fontSize="large" />;
   }
@@ -58,7 +72,7 @@ const AlertList = ({ alerts }: AlertListProps) => {
           <div className=" pr-6 text-[#30525E]">{alert.message}</div>
 
           {/* Type */}
-          <div className=" pl-2">{getAlertIcon(alert.message)}</div>
+          <div className=" pl-2">{getAlertIcon(alert.type)}</div>
 
           {/* Date */}
           <div className="text-[#30525E]">
