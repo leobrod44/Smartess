5fcd25629a1e5db7121a9b470883b21e32371481
layanat <98289247+layanat@users.noreply.github.com>
2025-02-09 10:55:26 -0500

    US 9.2 T2 T3 Alert List and Component (#481)
    * created layout of alert page and column labels #239
    
    * created alertlist and renderred it on alert page #240
    
    * fixed column spaces #240
    
    * changed mock data to match hub mocked data for alerts #240
    
    * used "type" data instead of  "message data" to render icons
    
    * changed alert component in indiividual unit page to match the alert types mocked by hub team #240
    
    * removed resolved from alerts #240
    
    * added pagination to alerts page #239
    
    * changed colors of icons #240
    
    * added unit number to table #240

--- smartessweb/frontend/src/app/dashboard/alerts/page.tsx
+++ smartessweb/frontend/src/app/dashboard/alerts/page.tsx
@@ -1,9 +1,72 @@
-const LockPage = () => {
+"use client";
+import AlertList from "../../components/AlertsPageComponents/AlertList";
+import { generateMockProjects } from "../../mockData";
+import Pagination from "@mui/material/Pagination";
+import { useState } from "react";
+
+const itemsPerPage = 8;
+
+const AlertPage = () => {
+  const alerts = generateMockProjects().flatMap((project) =>
+    project.units.flatMap((unit) => unit.alerts)
+  );
+
+  const [currentPage, setCurrentPage] = useState(1);
+  // Pagination logic
+  const totalPages = Math.ceil(alerts.length / itemsPerPage);
+  const currentItems = alerts.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
+
+  const handlePageChange = (
+    event: React.ChangeEvent<unknown>,
+    page: number
+  ) => {
+    setCurrentPage(page);
+  };
   return (
-    <div>
-      <h1>Alerts Page</h1>
+    <div className="flex border border-black rounded-lg p-6 mx-4 lg:mx-8 mt-6 min-h-screen flex-col">
+      <div className="flex items-center pt-4 justify-between mb-8">
+        <h1 className="w-full text-[#325a67] text-[30px] leading-10 tracking-tight whitespace-nowrap">
+          Alerts
+        </h1>
+      </div>
+      <div className="grid grid-cols-7 font-semibold border-b-2 border-black pb-2 mb-4">
+        <p className="pl-2 text-[#30525E] text-lg font-sequel-sans-medium leading-tight tracking-tight">
+          Id
+        </p>
+        <p className="pl-2 text-[#30525E] text-lg font-sequel-sans-medium leading-tight tracking-tight">
+          Unit
+        </p>
+        <p className="pl-2 text-[#30525E] text-lg font-sequel-sans-medium leading-tight tracking-tight">
+          Alert
+        </p>
+        <p className="pl-2 text-[#30525E] text-lg font-sequel-sans-medium leading-tight tracking-tight">
+          Type
+        </p>
+        <p className="pl-2 text-[#30525E] text-lg font-sequel-sans-medium leading-tight tracking-tight">
+          Date
+        </p>
+        <p className="pl-2 text-[#30525E] text-lg font-sequel-sans-medium leading-tight tracking-tight">
+          Time
+        </p>
+        <p className="pl-2 text-[#30525E] text-lg font-sequel-sans-medium leading-tight tracking-tight">
+          Action
+        </p>
+      </div>
+      <AlertList alerts={currentItems} />
+      <div className="mt-4 flex justify-center">
+        <Pagination
+          className="custom-pagination"
+          count={totalPages}
+          page={currentPage}
+          onChange={handlePageChange}
+          color="primary"
+        />
+      </div>
     </div>
   );
 };
 
-export default LockPage;
+export default AlertPage;
