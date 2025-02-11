9609fe501f45331b82e61a9ea5be4b6b0ec682f9
Matthew Flaherty <55712024+mattflahertyy@users.noreply.github.com>
2025-02-11 14:14:18 -0500

    BUG - Missing message for no hub users found (#490)
    updated message to display no hub users found #489

--- smartessweb/frontend/src/app/components/UnitComponents/HubUsers.tsx
+++ smartessweb/frontend/src/app/components/UnitComponents/HubUsers.tsx
@@ -5,7 +5,7 @@ interface HubUsersProps {
 }
 
 const HubUsers = ({ hubUsers }: HubUsersProps) => {
-  //only display first 3 users
+  // Only display first 3 users
   const displayedUsers = hubUsers.slice(0, 3);
 
   // Function to format the names as "M. Johnson"
@@ -14,6 +14,8 @@ const HubUsers = ({ hubUsers }: HubUsersProps) => {
       lastName.charAt(0).toUpperCase() + lastName.slice(1)
     }`;
   };
+
+  const hasHubUsers = hubUsers.length > 0;
 
   return (
     <div className="max-w-xs p-4 flex flex-col items-center gap-2.5">
@@ -24,16 +26,22 @@ const HubUsers = ({ hubUsers }: HubUsersProps) => {
         <div className="w-[75%] h-px absolute bg-[#4b7d8d] left-1/2 transform -translate-x-1/2" />
       </div>
       <div className="flex flex-col w-full gap-1">
-        <div className="flex-col justify-center items-center gap-1 inline-flex">
-          {displayedUsers.map((hubUsers, index) => (
-            <div
-              key={index}
-              className="text-black text-xs font-['Sequel Sans'] leading-tight tracking-tight text-center pb-1"
-            >
-              {formatUserName(hubUsers.firstName, hubUsers.lastName)}
-            </div>
-          ))}
-        </div>
+        {hasHubUsers ? (
+          <div className="flex-col justify-center items-center gap-1 inline-flex">
+            {displayedUsers.map((user, index) => (
+              <div
+                key={index}
+                className="text-black text-xs font-['Sequel Sans'] leading-tight tracking-tight text-center pb-1"
+              >
+                {formatUserName(user.firstName, user.lastName)}
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="text-center text-black text-xs font-medium">
+            No hub users found
+          </div>
+        )}
       </div>
     </div>
   );
