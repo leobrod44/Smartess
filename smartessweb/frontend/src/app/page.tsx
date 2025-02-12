6b2422b898a2e3cbe16c1b883e8f0abc4a889504
Layana Tounsi <98289247+layanat@users.noreply.github.com>
2025-02-11 20:26:34 -0500

    Task- F&F- landing page enhancement (#492)
    * made building image bigger #396
    
    * changed layouts to have products in one section and new space for a description #396
    
    * added smartess description #396
    
    * increased Z value of text container #396
    
    * changed start your project navigation to useRouter and fixed the Z index #396

--- smartessweb/frontend/src/app/page.tsx
+++ smartessweb/frontend/src/app/page.tsx
@@ -1,106 +1,125 @@
+"use client";
 import Image from "next/image";
 import desktop from "../public/images/desktop.png";
 import hub from "../public/images/hub.png";
 import building from "../public/images/building.png";
-import Link from "next/link";
 import Head from "next/head";
 import LandingNavbar from "@/app/components/LandingNavbar";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  const handleStartProject = () => {
+    router.push("/start-project");
+  };
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <LandingNavbar />
-      <main className="flex-col items-left justify-left space-y-16 pt-10">
-        <section className="text-center p-4 md:p-8">
+      <main className="flex-col items-left justify-left space-y-16 pt-10  ">
+        <section className="text-center p-4 md:p-8 relative z-[50]">
           <h1 className="text-3xl md:text-4xl font-bold">
             The Future of Smart Living is Here
           </h1>
+
           <p className="mt-4 text-base md:text-lg">
             First all-in-one smart home platform for residential communities
           </p>
-          <Link href="/start-project">
-            <button className="mt-6 px-6 py-3 bg-[#266472] text-white rounded-full hover:bg-[#1f505e] transition duration-300">
-              Start Your Project
-            </button>
-          </Link>
+          <button
+            onClick={handleStartProject}
+            className="  mt-6 px-6 py-3 bg-[#266472] text-white rounded-full hover:bg-[#1f505e] transition duration-300"
+          >
+            Start Your Project
+          </button>
         </section>
 
         <div className="relative w-full">
-          <section className="relative w-full h-[300px] flex flex-col md:flex-row justify-between items-center px-4 md:px-8 mt-16">
+          <section className="relative w-full h-[500px] flex flex-col md:flex-row justify-between items-center px-4 md:px-8 mt-16">
             <div
               className="absolute inset-0 bg-[#489ba7] z-10 h-full"
               style={{ top: "85px" }}
             ></div>
-            <div className=" hidden md:block mx-auto relative w-[200px] h-[300px] md:w-[200px] md:h-[400px] bg-[#E2E8F0] rounded-[30px] p-2 flex justify-center items-center mt-[-50px] z-20">
-              <Image
-                src={hub}
-                alt="Resident Mobile App"
-                className="hidden md:block mx-auto rounded-[20px]"
-                width={180}
-                height={360}
-              />
-            </div>
-
-            <div className="relative z-20 text-center md:text-left flex-1 px-4 mt-10 text-white pt-20 ">
-              <h2 className="text-xl md:text-2xl font-bold text-[#FFF]">
-                Resident Mobile App{" "}
+            <div className="relative z-[50] text-white max-w-lg pt-20 mr-10 mt-10 ">
+              <h1 className="text-2xl md:text-3xl font-bold pb-8">
+                Why Smartess?
+              </h1>
+              <h2 className="text-2xl md:text-2xl font-bold">
+                Smartess simplifies smart living by offering an all-in-one
+                solution that’s affordable, scalable, and easy to manage.
               </h2>
-              <p className="mt-5 text-sm md:text-lg z-40 ">
-                Control your smart devices as soon as you <br />
-                move in and from anywhere in the world.
-                <br />
-                Track your energy usage and receive alerts <br />
-                before disasters happen. Interact with <br />
-                other tenants, book amenities, and make
-                <br />
-                service requests for your unit.
+              <p className="mt-8 text-base md:text-lg">
+                Organizations and property owners benefit from a centralized
+                system to monitor and manage all their units and projects in one
+                place. No more juggling multiple providers or dealing with
+                devices that don’t work well together—everything just works
+                seamlessly.
               </p>
             </div>
 
-            <div className="hidden lg:flex justify-end pt-20 lg:pt-20 z-30 w-full absolute right-0">
+            <div className="hidden md:hidden lg:flex justify-end pt-20 lg:pt-2 lg:pb-[110px] z-30 w-full absolute right-0 ">
               <Image
                 src={building}
                 alt="Building"
                 layout="intrinsic"
-                width={400}
-                height={400}
+                width={800}
               />
             </div>
           </section>
         </div>
 
         <section
-          className="grid grid-cols-1 md:grid-cols-2 gap-3 md:gap-1 items-center w-full px-2 md:px-10 pb-10"
+          className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-16 items-center w-full px-4 md:px-10 pb-10 pt-10"
           style={{ marginTop: "150px", marginBottom: "50px" }}
         >
-          {/* Text section: On the left for larger screens, stacked on smaller screens */}
-          <div className="order-2 md:order-1">
-            <h2 className="text-xl md:text-2xl font-bold text-[#266472] pt-5">
+          {/* Left Section: Management Web App */}
+          <div className="bg-[#F7FAFC] p-10 rounded-lg shadow-lg">
+            <h2 className="text-xl md:text-2xl font-bold text-[#266472] pb-5">
               Management Web App
             </h2>
-            <p className="mt-4 text-base md:text-lg text-[#266472]">
+            <p className="text-base md:text-lg text-[#266472] pb-6">
               A centralized system that aggregates information from an owner’s
               units into a unified web platform. This platform offers invaluable
               insights, like the energy usage of each unit, and helps structure
               management processes.
             </p>
+            <div className="hidden md:hidden lg:flex justify-center">
+              <div className="relative w-[400px] h-[300px] md:w-[450px] md:h-[320px] bg-[#E2E8F0] rounded-[10px] p-6 flex justify-center items-center">
+                <Image
+                  src={desktop}
+                  alt="Owner Web App"
+                  width={450}
+                  height={350}
+                  layout="intrinsic"
+                />
+                {/* Laptop bottom */}
+                <div className="absolute bottom-[-12px] w-[100%] h-[12px] bg-gray-300 rounded-b-[10px] shadow-lg"></div>
+              </div>
+            </div>
           </div>
 
-          {/* Laptop mockup  */}
-          <div className="flex justify-center md:justify-end order-1 md:order-2">
-            <div className="relative w-[300px] h-[200px] md:w-[280px] md:h-[180px] bg-[#E2E8F0] rounded-[10px] p-3  flex justify-center items-center z-20 order-1 md:order-2">
-              <Image
-                src={desktop}
-                alt="Owner Web App"
-                width={280}
-                height={180}
-                layout="intrinsic"
-              />
-              {/* laptop bottom */}
-              <div className="absolute bottom-[-10px] w-[100%] h-[10px] bg-gray-300 rounded-b-[10px] shadow-lg"></div>
+          {/* Right Section: Resident Mobile App */}
+          <div className="bg-[#F7FAFC] p-8 rounded-lg shadow-lg">
+            <h2 className="text-xl md:text-2xl font-bold text-[#266472] pb-5">
+              Resident Mobile App
+            </h2>
+            <p className="text-base md:text-lg text-[#266472] pb-6">
+              Control your smart devices as soon as you move in and from
+              anywhere in the world. Track your energy usage and receive alerts
+              before disasters happen. Interact with other tenants, book
+              amenities, and make service requests for your unit.
+            </p>
+            <div className="hidden md:hidden lg:flex justify-center">
+              <div className="relative w-[200px] h-[300px] md:w-[200px] md:h-[400px] bg-[#E2E8F0] rounded-[30px] p-2 flex justify-center items-center">
+                <Image
+                  src={hub}
+                  alt="Resident Mobile App"
+                  className="rounded-[20px]"
+                  width={180}
+                  height={360}
+                />
+              </div>
             </div>
           </div>
         </section>
