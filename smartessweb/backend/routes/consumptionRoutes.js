fe97ad18b5305b98c4a2ec8b611b6edc0b0dbf28
TuanAnh-P <1tuananhp@gmail.com>
2025-02-15 01:13:57 -0500

    Retrieving energy consumption data given a hub id #389

--- smartessweb/backend/routes/consumptionRoutes.js
+++ smartessweb/backend/routes/consumptionRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { getConsumptions } = require("../controllers/consumptionController");
+const {
+  getConsumptions,
+  getConsumption,
+} = require("../controllers/consumptionController");
 
 const router = express.Router();
 
 router.get("/get_consumptions/:userId", getConsumptions);
+router.get("/get_consumption/:hubId", getConsumption);
 
 module.exports = router;
