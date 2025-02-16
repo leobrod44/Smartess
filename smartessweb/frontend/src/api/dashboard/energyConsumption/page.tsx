b650a45e64d169a2ba01e22a09bdf27a1a186e35
Tuan Anh Pham <98975548+TuanAnh-P@users.noreply.github.com>
2025-02-15 20:29:30 -0500

    US6.7-T1: Energy consumption component for each unit #326 (#484)
    * Energy consumption component for each unit #326
    
    * Display energy consumption element in a card-like layout on smaller screen size #326
    
    * Tooltip labels for energy consumption metrics #326

---
+++ smartessweb/frontend/src/api/dashboard/energyConsumption/page.tsx
@@ -0,0 +1,44 @@
+import { API_URL } from "@/api/api";
+
+export interface EnergyConsumptionsData {
+  energyConsumptionData: {
+    id: string;
+    proj_id: string;
+    hub_id: string;
+    unit_number: string;
+    created_at: string;
+    monthly_energy_consumption: number[];
+    monthly_temperature: number[];
+    daily_energy_consumption: number[];
+    address: string;
+    currentMonthConsumption: number;
+    currentMonthTemperature: number;
+    variation: number;
+  }[];
+}
+
+export const energyConsumptionApi = {
+  getEnergyConsumptions: async (
+    userId: string
+  ): Promise<EnergyConsumptionsData> => {
+    const response = await fetch(
+      `${API_URL}/consumptions/get_consumptions/${userId}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (!response.ok) {
+      const errorMessage = await response.json();
+      throw new Error(
+        errorMessage.error || "Failed to fetch energy consumption data"
+      );
+    }
+
+    const data: EnergyConsumptionsData = await response.json();
+    return data;
+  },
+};
