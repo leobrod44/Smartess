b3257dbbd1c3e868b35028c1c2b43ee68a4e8488
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2025-02-17 12:37:27 -0500

    #507 updating all contact buttons to have the same style (#524)
    #507 updating all contact button s to have the same hover effect, colr, and style

--- smartessweb/frontend/src/app/components/IndividualTicketPageComponents/UserComponents/AssignedUserClosedComponent.tsx
+++ smartessweb/frontend/src/app/components/IndividualTicketPageComponents/UserComponents/AssignedUserClosedComponent.tsx
@@ -1,5 +1,6 @@
 import { Individual } from "@/app/mockData";
 import React from "react";
+import { ChatBubbleLeftEllipsisIcon } from "@heroicons/react/24/solid";
 interface AssignedUserProps {
   Individual: Individual;
 }
@@ -16,8 +17,9 @@ function AssignedUserClosedTicket({ Individual }: AssignedUserProps) {
       </div>
 
       <div className="flex-1 flex justify-center">
-        <button className="py-1 bg-[#729987] rounded-[20px] justify-center items-center hover:bg-[#5C7A6B] transition duration-300 px-3  text-center text-white text-xs font-['Sequel Sans']">
+        <button className="w-[80px] h-[22px] flex items-center justify-center gap-2 bg-[#4b7d8d] rounded-md hover:bg-[#1f505e] transition duration-300 text-white text-xs font-['Sequel Sans'] leading-tight tracking-tight">
           Contact
+          <ChatBubbleLeftEllipsisIcon className="w-4 h-4" />
         </button>
       </div>
 
