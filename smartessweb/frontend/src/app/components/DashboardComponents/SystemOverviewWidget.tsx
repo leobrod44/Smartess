fe6e3c216e1b5bd2bcf62d633d83bcab5e4a7db7
Matthew Flaherty <matthewflaherty77@hotmail.com>
2025-02-17 13:45:59 -0500

    increase font by 1 size for system overview number #531

--- smartessweb/frontend/src/app/components/DashboardComponents/SystemOverviewWidget.tsx
+++ smartessweb/frontend/src/app/components/DashboardComponents/SystemOverviewWidget.tsx
@@ -12,22 +12,22 @@ const DashboardOverviewWidget = ({
     <div className="flex-col bg-[#14323B] w-full rounded-[7px] text-center m-0.5 p-1 hover:scale-105">
       <div className="flex flex-row w-full h-2/4 p-1">
         <div className="bg-[#325A67] w-full h-full place-content-around rounded-[7px] m-0.5">
-          <h3 className="text-2xl">{systemOverview?.projects}</h3>
+          <h3 className="text-3xl">{systemOverview?.projects}</h3>
           <h3 className="text-xs pb-1">Projects</h3>
         </div>
         <div className="bg-[#325A67] w-full h-full place-content-around rounded-[7px] m-0.5">
-          <h3 className="text-2xl">{systemOverview?.totalUnits}</h3>
+          <h3 className="text-3xl">{systemOverview?.totalUnits}</h3>
           <h3 className="text-xs pb-1 ">Total Units</h3>
         </div>
       </div>
 
       <div className="flex flex-row h-2/4 w-full p-1">
         <div className="bg-[#325A67] w-full h-full place-content-around rounded-[7px] m-0.5">
-          <h3 className="text-2xl">{systemOverview?.pendingTickets}</h3>
+          <h3 className="text-3xl">{systemOverview?.pendingTickets}</h3>
           <h3 className="text-xs pb-1">Pending Tickets</h3>
         </div>
         <div className="bg-[#325A67] w-full h-full place-content-around rounded-[7px] m-0.5">
-          <h3 className="text-2xl">{systemOverview?.totalAdminUsers}</h3>
+          <h3 className="text-3xl">{systemOverview?.totalAdminUsers}</h3>
           <h3 className="text-xs pb-1">Total Admin Users</h3>
         </div>
       </div>
