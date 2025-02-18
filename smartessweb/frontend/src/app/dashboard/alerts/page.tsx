a1901c1de1c80ac5888c1074266990b4aac82483
layanat <tounsilayana@gmail.com>
2025-02-17 23:13:51 -0500

    added search functionality #159

--- smartessweb/frontend/src/app/dashboard/alerts/page.tsx
+++ smartessweb/frontend/src/app/dashboard/alerts/page.tsx
@@ -3,18 +3,42 @@ import AlertList from "../../components/AlertsPageComponents/AlertList";
 import { generateMockProjects } from "../../mockData";
 import Pagination from "@mui/material/Pagination";
 import { useState } from "react";
+import Searchbar from "../../components/Searchbar";
 
 const itemsPerPage = 8;
 
 const AlertPage = () => {
-  const alerts = generateMockProjects().flatMap((project) =>
-    project.units.flatMap((unit) => unit.alerts)
+  const allAlerts = generateMockProjects().flatMap((project) =>
+    project.units.flatMap((unit) =>
+      unit.alerts.map((alert) => ({
+        ...alert,
+        unitNumber: unit.unitNumber, // Add unit number to each alert
+      }))
+    )
   );
+
+  const [filteredAlerts, setFilteredAlerts] = useState(allAlerts);
+
+  const handleSearch = (query: string) => {
+    const lowerCaseQuery = query.toLowerCase();
+
+    const filtered = allAlerts.filter((alert) => {
+      const messageMatch = new RegExp(`\\b${lowerCaseQuery}\\b`, "i").test(
+        alert.message
+      );
+      const unitMatch = alert.unitNumber.toString().includes(lowerCaseQuery);
+      const typeMatch = alert.type.toLowerCase() === lowerCaseQuery;
+
+      return messageMatch || unitMatch || typeMatch;
+    });
+    setFilteredAlerts(filtered);
+    setCurrentPage(1); // Reset pagination on search
+  };
 
   const [currentPage, setCurrentPage] = useState(1);
   // Pagination logic
-  const totalPages = Math.ceil(alerts.length / itemsPerPage);
-  const currentItems = alerts.slice(
+  const totalPages = Math.ceil(filteredAlerts.length / itemsPerPage);
+  const currentItems = filteredAlerts.slice(
     (currentPage - 1) * itemsPerPage,
     currentPage * itemsPerPage
   );
@@ -31,6 +55,9 @@ const AlertPage = () => {
         <h1 className="w-full text-[#325a67] text-[30px] leading-10 tracking-tight whitespace-nowrap">
           Alerts
         </h1>
+        <div className="flex items-center pt-2">
+          <Searchbar onSearch={handleSearch} />
+        </div>
       </div>
       <div className="grid grid-cols-7 font-semibold border-b-2 border-black pb-2 mb-4">
         <p className="pl-2 text-[#30525E] text-lg font-sequel-sans-medium leading-tight tracking-tight">
