fe491cb8010b62df4d3580ce364e5c03ff1bd1d7
Matthew Flaherty <matthewflaherty77@hotmail.com>
2025-02-18 15:42:52 -0500

    route for new controller #534

--- smartessweb/backend/routes/surveillanceRoutes.js
+++ smartessweb/backend/routes/surveillanceRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { getUserProjects} = require('../controllers/surveillanceController');
+const { getUserProjects, getProjectImages} = require('../controllers/surveillanceController');
 const { verifyToken } = require('../middleware/middleware');
 const router = express.Router();
 
 router.get('/get-user-projects', verifyToken, getUserProjects);
+router.get('/get-project-images', verifyToken, getProjectImages);
 
 module.exports = router;
