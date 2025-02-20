7e1237d29d90d598068e066543b7c87c78e8024f
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-02-20 15:53:38 -0500

    US 3.8 T5 Edit form and validation for password change (#542)
    * UI for manage password modal #244
    
    * Adding modal to profile component #244
    
    * Changing the title of the Modal for more clarity #244
    
    * Changes to state variables #244
    
    * Changes made error fix #244
    
    * Changes made for password visibility icon #244
    
    * Changing styling for button alignment #244

---
+++ smartessweb/frontend/src/app/components/ProfileComponents/ManagePasswordModal.tsx
@@ -0,0 +1,167 @@
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/solid";
+import React from "react";
+import { useState } from "react";
+import { showToastError, showToastSuccess } from "../Toast";
+
+const ManagePasswordModal = ({
+  isOpen,
+  onClose,
+  onResetPassword,
+}: {
+  isOpen: boolean;
+  onClose: () => void;
+  onResetPassword: (password: string) => void;
+}) => {
+  const [showNewPassword, setshowNewPassword] = useState(false);
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmNewPassword, setConfirmNewPassword] = useState("");
+
+  const handleNewPasswordInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setNewPassword(value);
+
+    // For backend and debugging
+    console.log(value);
+  };
+
+  const handleConfirmNewPasswordInput = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const value = e.target.value;
+    setConfirmNewPassword(value);
+  };
+
+  // Submit form with password
+  const handleFormSubmission = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    handleReset();
+  };
+
+  const handleReset = () => {
+    const passwordRegex = /^(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.{8,})/;
+
+    if (!newPassword || !confirmNewPassword) {
+      showToastError("Please fill in all required fields");
+      return;
+    }
+
+    if (!passwordRegex.test(newPassword)) {
+      showToastError(
+        "Password must be at least 8 characters long, include one capital letter, and one special character."
+      );
+      return;
+    }
+
+    if (newPassword !== confirmNewPassword) {
+      showToastError("Passwords do not match.");
+      return;
+    }
+
+    showToastSuccess("Reset email has been sent");
+    onResetPassword(confirmNewPassword);
+    setNewPassword("");
+    setConfirmNewPassword("");
+  };
+
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 bg-black/50 backdrop-blur-sm flex justify-center items-center z-50">
+      <form onSubmit={handleFormSubmission}>
+        <div className="bg-white rounded-xl shadow-lg p-10 w-[800px] h-[450px] relative">
+          <button
+            className="absolute top-3 right-3 text-gray-500 hover:text-gray-700 transition duration-300"
+            onClick={onClose}
+          >
+            ✕
+          </button>
+          <div className="flex justify-center">
+            <h2 className="text-4xl text-[#30525E] pt-10 font-sequel-sans font-extrabold">
+              Modify Password
+            </h2>
+          </div>
+          <div className="flex justify-center text-[#254752] text-xs mb-5">
+            <h3 className="text-sm text-[#52525C] pt-5 font-sequel-sans-regular">
+              Please create a new password.
+            </h3>
+          </div>
+          <div className="flex justify-center text-[#254752] text-xs mb-5">
+            <h3 className="text-sm text-[red] font-sequel-sans-regular">
+              Must be 8 characters long, include one capital letter and one
+              special character.
+            </h3>
+          </div>
+
+          <div className="w-4/5 mx-auto flex flex-col justify-center pb-10 space-y-4">
+            <div className="flex items-center gap-4">
+              <div className="w-1/3">
+                <label className="text-sm font-sequel-sans-regular text-right block">
+                  New Password
+                </label>
+              </div>
+              <input
+                type={showNewPassword ? "text" : "password"}
+                name="newPassword"
+                value={newPassword}
+                onChange={handleNewPasswordInput}
+                className="text-sm font-sequel-sans-regular border border-gray-400 rounded-lg px-3 py-1 w-2/4"
+              />
+            </div>
+            <div className="flex items-center gap-4">
+              <div className="w-1/3">
+                <label className="text-sm font-sequel-sans-regular text-right block">
+                  Confirm New Password
+                </label>
+              </div>
+
+              <div className="relative w-2/4">
+                <input
+                  type={showNewPassword ? "text" : "password"}
+                  name="confirmNewPassword"
+                  value={confirmNewPassword}
+                  onChange={handleConfirmNewPasswordInput}
+                  className="text-sm font-sequel-sans-regular border border-gray-400 rounded-lg px-3 py-1 pr-10 w-full"
+                />
+                <div>
+                  <button
+                    type="button"
+                    onClick={() => setshowNewPassword(!showNewPassword)}
+                    className="absolute right-3 top-1/2 transform -translate-y-1/2 text-[#266472] hover:text-[#1f505e] transition duration-300"
+                    aria-label={
+                      showNewPassword ? "Hide Password" : "Show password"
+                    }
+                  >
+                    {showNewPassword ? (
+                      <EyeSlashIcon className="h-6 w-6" />
+                    ) : (
+                      <EyeIcon className="h-6 w-6" />
+                    )}
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-center">
+            <div className="flex justify-around w-5/6">
+              <button
+                className="bg-[#ff5449] text-white text-xs w-[110px] py-2 rounded-md hover:bg-[#9b211b] transition duration-300"
+                type="button"
+                onClick={onClose}
+              >
+                Cancel
+              </button>
+              <button
+                className="bg-[#4b7d8d] text-white text-xs w-[110px] py-2 rounded-md hover:bg-[#254752] transition duration-300"
+                type="submit"
+              >
+                Confirm
+              </button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default ManagePasswordModal;
