b8d224ed5cf141dd2d4f49549417b3435ea3a48a
Leo Brodeur <leobrod44@gmail.com>
2025-02-26 23:54:58 -0500

    comment remove

--- go/hub/rtsp/rtsp.go
+++ go/hub/rtsp/rtsp.go
@@ -215,100 +215,6 @@ func (rtsp *RtspProcessor) Start() {
 	rtsp.Logger.Info("All RTSP streams have completed")
 }
 
-// ----------------------------------------------------------------------
-
-// streamURL := camera.StreamURL
-// rtsp.Logger.Info(fmt.Sprintf("stream url %s", streamURL))
-// rtsp.Logger.Info(fmt.Sprintf("Starting RTSP stream %s", camera.Name))
-// tmpDir := "/tmp/data/" + camera.Name
-// err := os.MkdirAll(tmpDir, 0755)
-// if err != nil {
-// 	rtsp.Logger.Error(fmt.Sprintf("Error creating directory %v: %v", tmpDir, err))
-// }
-// cmd := exec.Command("ffmpeg",
-// 	"-rtsp_transport", "tcp", // Use TCP for RTSP transport
-// 	"-i", streamURL, // Input RTSP stream
-// 	"-buffer_size", "1024000", // Set buffer size
-// 	"-probesize", "50M", // Increase probe size
-// 	"-analyzeduration", "20000000", // Increase analyze duration
-// 	"-avoid_negative_ts", "make_zero", // Avoid negative timestamps
-// 	"-c:v", "libx264", // Compress using H.264 codec
-// 	"-preset", "fast", // Use fast preset for encoding
-// 	"-crf", "23", // Control quality (lower = better)
-// 	"-r", "15", // Force frame rate
-// 	"-g", "30", // Set keyframe interval
-// 	"-b:a", "64k", // Set audio bitrate
-// 	"-f", "segment", // Enable segmentation
-// 	"-segment_time", "10", // Segment duration
-// 	"-reset_timestamps", "1", // Reset timestamps for each segment
-// 	"-segment_format", "mp4", // Output format
-// 	"-segment_list", fmt.Sprintf("%s/segments.m3u8", tmpDir), // HLS playlist
-// 	"-segment_list_type", "m3u8", // HLS format
-// 	"-segment_list_size", "0", // Unlimited segments
-// 	"-segment_list_flags", "+live", // Make it a live playlist
-// 	fmt.Sprintf("%s/segment-%%03d.mp4", tmpDir), // Segment output
-// )
-// time.Sleep(time.Second)
-
-// rtsp.Logger.Info(fmt.Sprintf("Starting FFmpeg for stream %s", camera.Name))
-// rtsp.Logger.Info(fmt.Sprintf("FFmpeg command: %v", cmd.String()))
-
-// ffmpegOutput := &bytes.Buffer{}
-// cmd.Stdout = ffmpegOutput
-// cmd.Stderr = ffmpegOutput
-
-// err = cmd.Start()
-// if err != nil {
-// 	rtsp.Logger.Error(fmt.Sprintf("Error starting FFmpeg for stream %s: %v", camera.Name, err))
-// }
-
-// go func(tmpDir string) {
-// 	for {
-// 		files, err := os.ReadDir(tmpDir)
-// 		if err != nil {
-// 			rtsp.Logger.Error(fmt.Sprintf("Error reading directory %v: %v", tmpDir, err))
-// 			return
-// 		}
-
-// 		for _, file := range files {
-// 			if file.IsDir() {
-// 				continue
-// 			}
-// 			if !strings.Contains(file.Name(), "segments.m3u8") {
-// 				rtsp.Logger.Info(fmt.Sprintf("Segment file: %v", file.Name()))
-// 				segmentFilePath := fmt.Sprintf("%s/%s", tmpDir, file.Name())
-
-// 				segmentFileContent, err := os.ReadFile(segmentFilePath)
-// 				if err != nil {
-// 					rtsp.Logger.Error(fmt.Sprintf("Error reading segment file %v: %v", segmentFilePath, err))
-// 					continue
-// 				}
-// 				rtsp.Logger.Info((fmt.Sprintf("Publishing segment to queue: %v", segmentFilePath)))
-// 				err = rtsp.instance.Channel.Publish(
-// 					"videostream", // Default exchange
-// 					fmt.Sprintf("videostream.hubid.%s", camera.Name), // Routing key (queue name)
-// 					false, // Mandatory
-// 					false, // Immediate
-// 					amqp.Publishing{
-// 						ContentType: "application/octet-stream", // Type of the content
-// 						Body:        segmentFileContent,         // Content (segment file data)
-// 					})
-// 				if err != nil {
-// 					rtsp.Logger.Error(fmt.Sprintf("Failed to publish segment to queue: %v", err))
-// 				}
-// 				err = os.Remove(segmentFilePath)
-// 				if err != nil {
-// 					rtsp.Logger.Error(fmt.Sprintf("Failed to remove segment file %v: %v", segmentFilePath, err))
-// 					continue
-// 				}
-// 				rtsp.Logger.Info(fmt.Sprintf("Published segment to queue: %v", segmentFilePath))
-// 			}
-// 		}
-
-// 		time.Sleep(time.Duration(camera.SegmentTime))
-// 	}
-// }(tmpDir)
-
 func (rtsp *RtspProcessor) Close() {
 	rtsp.client.Close()
 }
@@ -329,14 +235,12 @@ func (rtsp *RtspProcessor) newFFmpegConfig(camera CameraConfig) (*FFmpegConfig, 
 	streamURL := camera.StreamURL
 	tmpDir := "/tmp/data/" + camera.Name
 
-	// Create the temporary directory if it doesn't exist
 	err := os.MkdirAll(tmpDir, 0755)
 	if err != nil {
 		// Log and return error if directory creation fails
 		return nil, fmt.Errorf("error creating directory %v: %v", tmpDir, err)
 	}
 
-	// Return the FFmpeg configuration based on camera type
 	switch camera.Type {
 	case ANT, REAL:
 		return &FFmpegConfig{
@@ -404,29 +308,17 @@ func (rtsp *RtspProcessor) newFFmpegConfig(camera CameraConfig) (*FFmpegConfig, 
 			},
 			maxRetries:             5,
 			retryDelay:             5 * time.Second,
-			bufferPostSendInterval: 30 * time.Millisecond, // Adjusted buffer interval
-			bufferReaderSize:       1024 * 1024,           // Set buffer size for reading
+			bufferPostSendInterval: 30 * time.Millisecond,
+			bufferReaderSize:       1024 * 1024,
 		}, nil
 	default:
-		// Return an error if the camera type is invalid
 		return nil, fmt.Errorf("invalid camera selection: %d", camera.Type)
 	}
 }
 
 func (c *FFmpegConfig) buildCommand() *exec.Cmd {
 	args := append([]string{"-i", c.input}, c.options...)
-	args = append(args, "")
 	return exec.Command("ffmpeg", args...)
-}
-
-func validateRTSPStream(url string) error {
-	// Use ffprobe to validate the RTSP stream
-	cmd := exec.Command("ffprobe", "-v", "error", "-i", url, "-show_entries", "stream=codec_type", "-of", "default=noprint_wrappers=1:nokey=1")
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		return fmt.Errorf("stream validation failed: %v - output: %s", err, string(output))
-	}
-	return nil
 }
 
 func setupStreamEnvironment(camera string, rabbitMQURI string) (*stream.Environment, *stream.Producer, error) {
@@ -440,7 +332,7 @@ func setupStreamEnvironment(camera string, rabbitMQURI string) (*stream.Environm
 		return nil, nil, fmt.Errorf("failed to create stream environment: %v", err)
 	}
 
-	hubID := os.Getenv("HUB_ID")
+	hubID := os.Getenv("HUB_IP")
 	streamName := fmt.Sprintf("video_stream.hub_id.%s.%s", hubID, camera)
 
 	err = env.DeclareStream(streamName, &stream.StreamOptions{
