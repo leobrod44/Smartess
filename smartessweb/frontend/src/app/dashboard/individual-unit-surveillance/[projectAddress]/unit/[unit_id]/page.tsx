d072080190c951d25e352325b23bc5cfc67116ae
Matthew Flaherty <matthewflaherty77@hotmail.com>
2025-02-28 20:10:09 -0500

    added skeleton for individual unit surveillance page #572

--- smartessweb/frontend/src/app/dashboard/individual-unit-surveillance/[projectAddress]/unit/[unit_id]/page.tsx
+++ smartessweb/frontend/src/app/dashboard/individual-unit-surveillance/[projectAddress]/unit/[unit_id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 export default function UnitPage({
   params,
@@ -12,6 +12,8 @@ export default function UnitPage({
 
   const router = useRouter();
   const decodedAddress = decodeURIComponent(projectAddress);
+
+  const videoRef = useRef<HTMLVideoElement>(null);
 //   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -31,9 +33,58 @@ export default function UnitPage({
 //     );
 //   }
 
-  return (
-    <div>
-      Individual Unit Surveillance Page for <br/> {decodedAddress} <br/>Unit {unit_id}
+return (
+  <div>
+    <div className="flex-1 border border-black rounded-lg p-6 mx-4 lg:mx-8 min-h-screen flex flex-col">
+      {/* Back Arrow Button */}
+      <div className="flex items-center justify-between mb-4">
+        <h1 className="text-[#325a67] text-[35px] leading-10 tracking-tight">
+          {decodedAddress}
+        </h1>
+      </div>
+      <h1 className="text-[#729987] text-[25px] leading-10 tracking-tight">
+        Unit {unit_id}
+      </h1>
+      
+      {/* Video Section */}
+      <div className="my-5 flex justify-center">
+        <div className="rounded-lg bg-[#4b7d8d] p-2 w-full max-w-2xl relative">
+          <div className="bg-white rounded-lg p-2">
+            <video 
+              ref={videoRef} 
+              className="w-full border border-gray-300 rounded-lg"
+              controls 
+              autoPlay 
+              playsInline 
+            />
+          </div>
+
+
+        </div>
+      </div>
+
+      <div className="border border-black rounded-lg mt-4 bg-[#4b7d8d]">
+        <div className="grid grid-cols-2 gap-4 h-full">
+          {/* Speed Section */}
+          <div className="rounded-lg p-2 cursor-pointer hover:bg-[#325a67] flex min-h-[150px]">
+            <div className="bg-white rounded-lg p-4 text-center flex flex-col justify-center w-full h-full">
+              <h2 className="text-xl font-bold">Speed</h2>
+              <p className="text-lg">0 kbps</p>
+            </div>
+          </div>
+
+          {/* Buffer Health Section */}
+          <div className="rounded-lg p-2 cursor-pointer hover:bg-[#325a67] flex min-h-[150px]">
+            <div className="bg-white rounded-lg p-4 text-center flex flex-col justify-center w-full h-full">
+              <h2 className="text-xl font-bold">Buffer Health</h2>
+              <p className="text-lg">0 sec</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
-  );
+  </div>
+);
+
 }
