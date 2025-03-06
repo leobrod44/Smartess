5f01beab1c4968f169418893812705eb29799e00
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2025-03-06 15:59:03 -0500

    General enhancement: adding min-h screen  (#600)
    adding minimum page height to surveillance and unit page. Other pages had necessary heights #530.

--- smartessweb/frontend/src/app/dashboard/surveillance/page.tsx
+++ smartessweb/frontend/src/app/dashboard/surveillance/page.tsx
@@ -3,11 +3,11 @@
 import { useProjectContext } from "@/context/ProjectProvider";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { /*generateMockSurveillanceCameras, */Project } from "../../mockData";
+import { /*generateMockSurveillanceCameras, */ Project } from "../../mockData";
 import { surveillanceApi } from "@/api/page";
 import Searchbar from "@/app/components/Searchbar";
 import { IconButton, Pagination } from "@mui/material";
-import PlayArrow from '@mui/icons-material/PlayArrow';
+import PlayArrow from "@mui/icons-material/PlayArrow";
 import SurveillanceWidget from "@/app/components/SurveillanceComponents/SurveillanceWidget";
 import Image from "next/image";
 
@@ -17,11 +17,15 @@ const SurveillancePage = () => {
   // const mockData = generateMockSurveillanceCameras();
   const [isLoading, setIsLoading] = useState(true);
   const [projects, setProjects] = useState<Project[]>([]);
-  const [projectImages, setProjectImages] = useState<{ [projectId: string]: string }>({});
+  const [projectImages, setProjectImages] = useState<{
+    [projectId: string]: string;
+  }>({});
   const { selectedProjectAddress } = useProjectContext();
   const [searchQuery, setSearchQuery] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
-  const [widgetFilter, setWidgetFilter] = useState<"all" | "live" | "disconnected">("all");
+  const [widgetFilter, setWidgetFilter] = useState<
+    "all" | "live" | "disconnected"
+  >("all");
 
   const unitsPerPage = 4;
 
@@ -33,7 +37,7 @@ const SurveillancePage = () => {
       return;
     }
 
-const fetchData = async () => {
+    const fetchData = async () => {
       setIsLoading(true);
 
       try {
@@ -43,13 +47,13 @@ const fetchData = async () => {
         const responseImages = await surveillanceApi.getProjectImages(token);
 
         const imagesMap = responseImages.images.reduce((acc, imageUrl) => {
-          const fileName = imageUrl.split("/").pop()?.split(".")[0]; 
+          const fileName = imageUrl.split("/").pop()?.split(".")[0];
           if (fileName) {
             acc[fileName] = imageUrl;
           }
           return acc;
         }, {} as { [projectId: string]: string });
-    
+
         setProjectImages(imagesMap);
       } catch (error) {
         console.error("Error fetching surveillance data:", error);
@@ -69,30 +73,30 @@ const fetchData = async () => {
     }))
   );
 
-  const filteredUnitsByProject = allUnits.filter((unit) => {  
+  const filteredUnitsByProject = allUnits.filter((unit) => {
     const effectiveProjectAddress = selectedProjectAddress || "ALL PROJECTS";
     const normalizedSearchQuery = searchQuery.toLowerCase().trim();
-  
+
     const matchesProjectAddress =
       effectiveProjectAddress === "ALL PROJECTS" ||
       unit.projectAddress === effectiveProjectAddress;
-  
+
     const matchesSearchQuery =
       unit.projectAddress.toLowerCase().includes(normalizedSearchQuery) ||
       String(unit.unitNumber).toLowerCase().includes(normalizedSearchQuery) ||
       `unit ${unit.unitNumber}`.toLowerCase().includes(normalizedSearchQuery);
-  
+
     return matchesProjectAddress && matchesSearchQuery;
   });
 
   const filteredByCameraStatus = filteredUnitsByProject.filter((unit) => {
     if (widgetFilter === "all") return true;
     if (widgetFilter === "live" && unit.cameraStatus === "live") return true;
-    if (widgetFilter === "disconnected" && (unit.cameraStatus === "disconnected")) return true;
+    if (widgetFilter === "disconnected" && unit.cameraStatus === "disconnected")
+      return true;
     return false;
   });
 
-  
   const handleSearch = (query: string) => {
     setSearchQuery(query);
   };
@@ -104,7 +108,10 @@ const fetchData = async () => {
     setCurrentPage(page);
   };
 
-  const handleViewIndividualUnitSurveillance = (projectAddress: string, unitNumber: string) => {
+  const handleViewIndividualUnitSurveillance = (
+    projectAddress: string,
+    unitNumber: string
+  ) => {
     router.push(
       `../dashboard/individual-unit-surveillance/${projectAddress}/unit/${unitNumber}`
     );
@@ -113,23 +120,27 @@ const fetchData = async () => {
   const totalPages = Math.ceil(filteredByCameraStatus.length / unitsPerPage);
 
   const currentUnits =
-  filteredByCameraStatus.length === 0
-      ? [] 
+    filteredByCameraStatus.length === 0
+      ? []
       : filteredByCameraStatus.slice(
           (currentPage - 1) * unitsPerPage,
           currentPage * unitsPerPage
         );
 
-  const liveCamerasCount = allUnits.filter((unit) => unit.cameraStatus === "live").length;
-  const inactiveCamerasCount = allUnits.filter((unit) => unit.cameraStatus === "disconnected").length;
+  const liveCamerasCount = allUnits.filter(
+    (unit) => unit.cameraStatus === "live"
+  ).length;
+  const inactiveCamerasCount = allUnits.filter(
+    (unit) => unit.cameraStatus === "disconnected"
+  ).length;
 
   const handleClickTotal = () => setWidgetFilter("all");
   const handleClickLive = () => setWidgetFilter("live");
   const handleClickDisconnected = () => setWidgetFilter("disconnected");
-        
+
   return (
     <div>
-      <div className="border border-black rounded-lg p-6 mx-4 lg:mx-8">
+      <div className="border border-black rounded-lg p-6 mx-4 lg:mx-8 min-h-screen">
         <div className="flex flex-row justify-between">
           <h2 className="text-left text-[#325a67] text-[30px] leading-10 tracking-tight">
             Surveillance Page
@@ -163,7 +174,7 @@ const fetchData = async () => {
             </div>
           </div>
         )}
-        { isLoading ? (
+        {isLoading ? (
           <p className="text-[#729987] text-xl font-sequel-sans-black text-center p-2">
             Loading surveillance data...
           </p>
@@ -175,14 +186,23 @@ const fetchData = async () => {
               Please adjust your filters or search criteria.
             </p>
           </div>
-        ) : ( 
+        ) : (
           <div className="grid grid-cols-2 gap-4 mt-3">
             {currentUnits.slice(0, unitsPerPage).map((unit) => (
-              <div key={unit.unitNumber} className="border p-2 bg-[#4b7d8d] rounded-lg cursor-pointer relative" onClick={() => handleViewIndividualUnitSurveillance(unit.projectAddress, unit.unitNumber)}>
+              <div
+                key={unit.unitNumber}
+                className="border p-2 bg-[#4b7d8d] rounded-lg cursor-pointer relative"
+                onClick={() =>
+                  handleViewIndividualUnitSurveillance(
+                    unit.projectAddress,
+                    unit.unitNumber
+                  )
+                }
+              >
                 <div className="relative">
                   {projectImages[unit.projectId.toString()] && (
                     <Image
-                      src={projectImages[unit.projectId.toString()]} 
+                      src={projectImages[unit.projectId.toString()]}
                       alt={`Project ${unit.projectId}`}
                       width={300}
                       height={300}
@@ -191,24 +211,31 @@ const fetchData = async () => {
                     />
                   )}
                   <div className="absolute inset-0 flex items-center justify-center">
-                    <IconButton sx={{ backgroundColor: "rgba(55, 65, 81, 0.5)", borderRadius: "50%", padding: "12px" }}>
+                    <IconButton
+                      sx={{
+                        backgroundColor: "rgba(55, 65, 81, 0.5)",
+                        borderRadius: "50%",
+                        padding: "12px",
+                      }}
+                    >
                       <PlayArrow sx={{ color: "white", fontSize: "48px" }} />
                     </IconButton>
                   </div>
                 </div>
-              <div className="mt-2 flex justify-between text-white">
-                <p className="text-sm">{unit.projectAddress}</p>
-                <p className="text-sm flex items-center">
-                  Unit {unit.unitNumber}
-                  <span
-                    className={`w-3 h-3 rounded-full ml-2 ${
-                      unit.cameraStatus === "live" ? "bg-green-500" : "bg-red-500"
-                    }`}
-                  />
-                </p>
+                <div className="mt-2 flex justify-between text-white">
+                  <p className="text-sm">{unit.projectAddress}</p>
+                  <p className="text-sm flex items-center">
+                    Unit {unit.unitNumber}
+                    <span
+                      className={`w-3 h-3 rounded-full ml-2 ${
+                        unit.cameraStatus === "live"
+                          ? "bg-green-500"
+                          : "bg-red-500"
+                      }`}
+                    />
+                  </p>
+                </div>
               </div>
-            </div>
-            
             ))}
           </div>
         )}
@@ -224,7 +251,6 @@ const fetchData = async () => {
       </div>
     </div>
   );
-
 };
 
 export default SurveillancePage;
