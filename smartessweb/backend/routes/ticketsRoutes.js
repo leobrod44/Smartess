3ffcc97c5d8a08e8d3f9951282ba22f262c29562
Tuan Anh Pham <98975548+TuanAnh-P@users.noreply.github.com>
2025-03-07 18:40:03 -0500

    TASK: Notification System for ticket assignment and resolution (#606)
    * Keeping track of user who assigned tickets #605
    
    * creating a notification whenever assigning ticket or changing its resolution status #605
    
    * Creating notification to users assigned to tickets when they have been unassigned #605
    
    * API for getting ticket notifications #605
    
    * Retrieving and displaying ticket notifications on frontend #605
    
    * When an user clicks on a ticket notification, the notification status goes from unseen to seen and redirects the user that individual ticket page #605
    
    * Fix eslint error #605

--- smartessweb/backend/routes/ticketsRoutes.js
+++ smartessweb/backend/routes/ticketsRoutes.js
@@ -1,20 +1,49 @@
-const express = require('express');
-const { getTickets, deleteTicket, fetchIndividualTicket, getAssignableEmployees, getAssignedUsers, assignUsersToTicket, unassignUserFromTicket, closeTicket,
-     getAssignedTicketsForUser, 
-     updateTicketResolutionStatus} = require('../controllers/ticketsController');
-const { verifyToken } = require('../middleware/middleware');
+const express = require("express");
+const {
+  getTickets,
+  deleteTicket,
+  fetchIndividualTicket,
+  getAssignableEmployees,
+  getAssignedUsers,
+  assignUsersToTicket,
+  unassignUserFromTicket,
+  closeTicket,
+  getAssignedTicketsForUser,
+  updateTicketResolutionStatus,
+  getTicketNotifications,
+  updateTicketNotification,
+} = require("../controllers/ticketsController");
+const { verifyToken } = require("../middleware/middleware");
 
 const router = express.Router();
 
-router.get('/get-tickets', verifyToken, getTickets);
-router.delete('/delete-ticket/:ticket_id', verifyToken, deleteTicket);
-router.get('/ticket/:ticket_id', verifyToken, fetchIndividualTicket);
-router.get('/assignable-employees/:ticket_id', verifyToken, getAssignableEmployees);
-router.get('/assigned-users/:ticket_id', verifyToken, getAssignedUsers);
-router.post('/assign-users', verifyToken, assignUsersToTicket);
-router.post('/unassign-user', verifyToken, unassignUserFromTicket);
-router.post('/close-ticket/:ticket_id', verifyToken, closeTicket);
-router.get('/get-assigned-tickets-for-user', verifyToken, getAssignedTicketsForUser);
-router.post('/update-ticket-resolution', verifyToken, updateTicketResolutionStatus);
+router.get("/get-tickets", verifyToken, getTickets);
+router.delete("/delete-ticket/:ticket_id", verifyToken, deleteTicket);
+router.get("/ticket/:ticket_id", verifyToken, fetchIndividualTicket);
+router.get(
+  "/assignable-employees/:ticket_id",
+  verifyToken,
+  getAssignableEmployees
+);
+router.get("/assigned-users/:ticket_id", verifyToken, getAssignedUsers);
+router.post("/assign-users", verifyToken, assignUsersToTicket);
+router.post("/unassign-user", verifyToken, unassignUserFromTicket);
+router.post("/close-ticket/:ticket_id", verifyToken, closeTicket);
+router.get(
+  "/get-assigned-tickets-for-user",
+  verifyToken,
+  getAssignedTicketsForUser
+);
+router.post(
+  "/update-ticket-resolution",
+  verifyToken,
+  updateTicketResolutionStatus
+);
+router.get("/get-ticket-notifications", verifyToken, getTicketNotifications);
+router.post(
+  "/update-ticket-notification",
+  verifyToken,
+  updateTicketNotification
+);
 
 module.exports = router;
