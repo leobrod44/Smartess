52ce39beca98b5f785ab3ce2ffa2f47e887f923a
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2025-03-10 11:51:26 -0400

    linting #529

--- smartessweb/frontend/src/app/components/NoResultsFound.tsx
+++ smartessweb/frontend/src/app/components/NoResultsFound.tsx
@@ -10,7 +10,7 @@ const NoResultsFound= ({ searchItem }: NoResults) => {
         <>
         <div className="w-full flex flex-col items-center justify-center gap-2">
             <Image src={noresultsimg} alt="No results found" width={240} height={200} />
-           <h2 className=" text-2xl text-[#325a67]"> We're Sorry...</h2>
+           <h2 className=" text-2xl text-[#325a67]"> We&apos;re Sorry...</h2>
            <h3> We couldn&apos;t find any matches for <span className="text-[#325a67]">&quot;{searchItem}&quot;</span></h3>
            <h5>Double check your search for any typos, or please try a different search term</h5>
           
