8aa06387e861cf0e6645f86628325a7d807564c7
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2025-03-11 10:19:42 -0400

    Adding page description to energy consumtipn page, removed border #567

--- smartessweb/frontend/src/app/dashboard/consumption/page.tsx
+++ smartessweb/frontend/src/app/dashboard/consumption/page.tsx
@@ -157,8 +157,8 @@ const ConsumptionPage = () => {
   }
 
   return (
-    <div className="border border-black rounded-lg p-6 mx-4 lg:mx-8 mt-6 min-h-screen flex flex-col">
-      <div className="flex items-center justify-between pb-4">
+    <div className=" mx-4 lg:mx-8 min-h-screen flex flex-col">
+      <div className="flex items-center justify-between">
         <h1 className="text-[#325a67] text-[30px] leading-10 tracking-tight">
           Energy Consumption
         </h1>
@@ -168,8 +168,10 @@ const ConsumptionPage = () => {
             filterOptions={filterOptionsConsumption}
           />
           <Searchbar onSearch={handleSearch} />
-        </div>
+        </div> 
       </div>
+      <h2 className="text-left text-[#325a67] text-[16px] leading-2 tracking-tight"> View and manage the energy consumption data across all units in your organization. Use the Project Filter on the left or the search bar  and filter above to narrow down results.</h2>
+       
       <EnergyConsumptionComponent
         energyConsumptions={filteredEnergyConsumptions}
         query={query}
