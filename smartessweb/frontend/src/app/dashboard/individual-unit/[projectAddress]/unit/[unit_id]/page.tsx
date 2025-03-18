08328f9fbb6701aa7b899dc2d86a5a8ef55291b4
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-03-18 17:25:13 -0400

    Task  General Enhancement for Loading Screens (#639)
    * Changed Dashboard loading screen for consistency #457
    
    * Changed Announcement loading screen for consistency #457
    
    * Changed Consumption loading screen for consistency #457
    
    * Changed Manage-Accounts loading screen for consistency #457
    
    * Changed Surveillance loading screen for consistency #457
    
    * Changed Unit-List loading screen for consistency #457
    
    * Changed Individual-Unit loading screen for consistency #457

--- smartessweb/frontend/src/app/dashboard/individual-unit/[projectAddress]/unit/[unit_id]/page.tsx
+++ smartessweb/frontend/src/app/dashboard/individual-unit/[projectAddress]/unit/[unit_id]/page.tsx
@@ -77,8 +77,8 @@ export default function UnitPage({
 
   if (loading) {
     return (
-      <div className="flex justify-center items-center h-screen">
-        <div className="text-[#14323B] text-lg">Loading projects...</div>
+      <div className="flex items-center justify-center h-screen">
+        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900"></div>
       </div>
     );
   }
