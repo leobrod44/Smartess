c65f68f7022917dda502782b5049d0bebd9bdf71
ChiefsBestPal <67126866+ChiefsBestPal@users.noreply.github.com>
2025-03-22 02:10:48 -0400

    Impl V1 Video Consumer handler

--- go/server/rabbitmq/handlers/video_controller.go
+++ go/server/rabbitmq/handlers/video_controller.go
@@ -1,9 +1,12 @@
 package handlers
 
 import (
+	"Smartess/go/common/logging"
 	common_rabbitmq "Smartess/go/common/rabbitmq"
 	"fmt"
+	"github.com/gorilla/websocket"
 	"log"
+	"net/http"
 
 	stream_amqp "github.com/rabbitmq/rabbitmq-stream-go-client/pkg/amqp"
 	"github.com/rabbitmq/rabbitmq-stream-go-client/pkg/stream"
@@ -11,16 +14,75 @@ import (
 	"go.uber.org/zap"
 )
 
+var upgrader = websocket.Upgrader{
+	//ReadBufferSize:  1024,
+	//WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+var clients = make(map[*websocket.Conn]bool)
+
+// var broadcast = make(chan []byte)
+//
+// // Broadcast MP4 segments to all connected WebSocket clients
+//
+//	func broadcastSegment(data []byte) {
+//		broadcast <- data
+//	}
+//
+//	func handleWS(w http.ResponseWriter, r *http.Request) {
+//		conn, err := upgrader.Upgrade(w, r, nil)
+//		if err != nil {
+//			log.Println("Failed to upgrade WebSocket:", err)
+//			return
+//		}
+//		defer conn.Close()
+//		clients[conn] = true
+//		log.Println("New WebSocket connection")
+//
+//		// Keep connection alive
+//		for {
+//			if _, _, err := conn.ReadMessage(); err != nil {
+//				log.Println("WebSocket closed:", err)
+//				delete(clients, conn)
+//				break
+//			}
+//		}
+//	}
+func (h *ControllerHandler) handleWS(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		h.videoLogger.Error(fmt.Sprintf("Failed to upgrade WebSocket: %v\n\r", err))
+		return
+	}
+	defer conn.Close()
+	clients[conn] = true
+	h.videoLogger.Info("New WebSocket connection\n\r")
+
+	// Keep connection alive
+	for {
+		if _, _, err := conn.ReadMessage(); err != nil {
+			h.videoLogger.Error(fmt.Sprintf("Websocket closed: %v\n\r", err))
+			delete(clients, conn)
+			break
+		}
+	}
+}
+
 type ControllerHandler struct {
-	r   *common_rabbitmq.RabbitMQInstance
-	env *stream.Environment
+	r           *common_rabbitmq.RabbitMQInstance
+	env         *stream.Environment
+	videoLogger *zap.Logger
 }
 
 func NewControllerHandler(instance *common_rabbitmq.RabbitMQInstance, env *stream.Environment) *ControllerHandler {
-	return &ControllerHandler{r: instance, env: env}
+	zapLogger, err := logging.InitializeLogger("/app/logs/server_video.log")
+	if err != nil {
+		log.Fatalf("Failed to initialize server video zap logger: %v", err)
+	}
+	return &ControllerHandler{r: instance, env: env, videoLogger: zapLogger}
 }
 
-// TODO Using log instead of zap or custom output log... cant see logs
 func (h *ControllerHandler) Handle(msg amqp.Delivery, logger *zap.Logger) {
 	streamName := string(msg.Body)
 	logger.Info("Received stream name", zap.String("stream_name", streamName))
@@ -32,7 +94,7 @@ func (h *ControllerHandler) Handle(msg amqp.Delivery, logger *zap.Logger) {
 	if err != nil {
 		log.Fatalf("Failed to declare a stream: %v", err)
 	}
-	log.Printf("Declared stream: %s", streamName)
+	h.videoLogger.Info(fmt.Sprintf("Declared stream: %s", streamName))
 
 	// Consumer handler
 	messagesHandler := func(consumerContext stream.ConsumerContext, message *stream_amqp.Message) {
@@ -42,7 +104,7 @@ func (h *ControllerHandler) Handle(msg amqp.Delivery, logger *zap.Logger) {
 		if message == nil {
 			logMessage = "Message is nil"
 		} else if message.Properties == nil {
-			logMessage = "Message Properties is nil"
+			logMessage = fmt.Sprintf("ValuePayload:%v", message.Value)
 		} else {
 			logMessage = fmt.Sprintf("MsgID: %v, UserID: %x, To: %s, Subject: %s, ReplyTo: %s",
 				message.Properties.MessageID,
@@ -52,11 +114,25 @@ func (h *ControllerHandler) Handle(msg amqp.Delivery, logger *zap.Logger) {
 				message.Properties.ReplyTo)
 		}
 
-		log.Printf("[TEMP CONSUME] Stream:%s|%s", streamName, logMessage)
+		h.videoLogger.Info(fmt.Sprintf("[TEMP CONSUME] Stream:%s|%s", streamName, logMessage))
+		//TODO: broacast_gochan based "init() go func() for for" for data := range broadcast {
+		for client := range clients {
+			// Flatten the [][]byte to a single []byte
+			var flattenedData []byte
+			for _, chunk := range message.Data {
+				flattenedData = append(flattenedData, chunk...)
+			}
+			err := client.WriteMessage(websocket.BinaryMessage, flattenedData)
+			if err != nil {
+				h.videoLogger.Error(fmt.Sprintf("Error sending data to websocket: %v", err))
+				client.Close()
+				delete(clients, client)
+			}
+		}
 	}
 	// goroutine for MQ stream consumers
 	go func() {
-		_, err := h.env.NewConsumer(streamName, messagesHandler,
+		consumer, err := h.env.NewConsumer(streamName, messagesHandler,
 			stream.NewConsumerOptions().SetOffset(stream.OffsetSpecification{}.First()))
 		if err != nil {
 			logger.Fatal("Failed to create consumer", zap.Error(err))
@@ -65,8 +141,15 @@ func (h *ControllerHandler) Handle(msg amqp.Delivery, logger *zap.Logger) {
 
 		logger.Info("Consumer started", zap.String("stream_name", streamName))
 
-		select {} // keep the goroutine alive
-		// TODO WEBSOCKETS + CONSUMER CLOSE
+		// select {} // keep goroutine alive
+
+		//TODO WEBSOCKET + CLOSE CONSUMER
+		defer consumer.Close()
+
+		// WebSocket server
+		http.HandleFunc("/ws", h.handleWS)
+		h.videoLogger.Info("WebSocket server (Video consumer) started on :8080\n\r")
+		log.Fatal(http.ListenAndServe(":8080", nil))
 	}()
 
 }
