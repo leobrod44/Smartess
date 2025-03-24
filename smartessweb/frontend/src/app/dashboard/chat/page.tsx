854a5b3c857fec4dcabcad76fb5a9f4a8c16d5b8
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2025-03-24 11:09:42 -0400

    #566 chat page coming soon banner

--- smartessweb/frontend/src/app/dashboard/chat/page.tsx
+++ smartessweb/frontend/src/app/dashboard/chat/page.tsx
@@ -1,7 +1,23 @@
+"use client";
+
+import BackArrowButton from "@/app/components/BackArrowBtn";
+
 const ChatPage = () => {
   return (
-    <div>
-      <h1>Chat Page</h1>
+    <div className="relative h-screen ">
+      <div className="px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between pb-4">
+          <div className="pt-4 w-[306px] h-[66px] text-[#325a67] text-[30px] leading-2 tracking-tight">
+            Chat Page
+          </div>
+          <BackArrowButton />
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center justify-center h-full bg-gradient-to-b from-white to-[#1f505e]">
+        <p className="text-sm text-gray-300 mb-2">This feature is</p>
+        <h2 className="text-4xl font-bold text-white">Coming Soon</h2>
+      </div>
     </div>
   );
 };
