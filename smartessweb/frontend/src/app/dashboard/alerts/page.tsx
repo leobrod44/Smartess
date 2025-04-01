4dc58d9831496583f906d91f56e839ae37b8d714
Ryan Li <99624359+Ryan2Li@users.noreply.github.com>
2025-04-01 12:53:59 -0400

    credentials and fixes (#688)

--- smartessweb/frontend/src/app/dashboard/alerts/page.tsx
+++ smartessweb/frontend/src/app/dashboard/alerts/page.tsx
@@ -22,7 +22,7 @@ const AlertPage = () => {
   // Update this useMemo to depend on projects state instead of mockProjects
   const allAlerts = useMemo(() => {
     const uniqueAlerts = new Map();
-  
+
     projects.forEach((project) => {
       project.units.forEach((unit) => {
         unit.alerts.forEach((alert) => {
@@ -32,6 +32,7 @@ const AlertPage = () => {
         });
       });
     });
+    console.log(projects)
     return Array.from(uniqueAlerts.values());
   }, [projects]);
 
@@ -92,17 +93,20 @@ const AlertPage = () => {
           const unitIndex = project.units.findIndex(unit => unit.hubIp === incomingAlert.hub_ip);
           if (unitIndex == -1) {
             console.log('No matching hub found for:', incomingAlert.hub_ip);
-            return updatedProjects; // Return unchanged state
+            continue;
+          }
+          if (unitIndex == -1) {
+            return updatedProjects;
           }
 
           const unit = project.units[unitIndex]
 
           const newAlert : Alert = {
-            id: "9999999",
+            id: Math.floor(10000 + Math.random() * 90000).toString(),
             projectId: project.projectId,
             unitNumber: unit.unitNumber,
             description: incomingAlert.state,
-            message: incomingAlert.message,
+            message: incomingAlert.type,
             active: true, // Assuming new alerts start as active
             type: incomingAlert.type,
             timestamp: incomingAlert.time_fired,
@@ -111,7 +115,12 @@ const AlertPage = () => {
           };
           // Add the new alert to this unit's alerts array
           unit.alerts = [...unit.alerts, newAlert];
-
+          unit.alerts = [
+            ...unit.alerts.filter(
+                (alert) => !(alert.timestamp === newAlert.timestamp && alert.type === newAlert.type && alert.deviceId === newAlert.deviceId)
+            ),
+            newAlert,
+          ];
           return updatedProjects; // Return updated state
         }
 
@@ -119,20 +128,19 @@ const AlertPage = () => {
         console.log('No matching hub found for:', incomingAlert.hub_ip);
         return updatedProjects; // Return unchanged state
       });
-
     } catch (error) {
       console.error('Error processing alert:', error);
     }
   };
 
-    // WebSocket implementation with @stomp/stompjs
+  // WebSocket implementation with @stomp/stompjs
   useEffect(() => {
     // Create a new STOMP client
     const client = new Client({
-      brokerURL: "ws://localhost:15674/ws", // Use wss:// for secure connection
+      brokerURL: 'ws://151.145.39.229:15674/ws',
       connectHeaders: {
-        login: "admin",
-        passcode: "admin",
+        login: 'admin',
+        passcode: 'admin'
       },
       debug: function (str) {
         console.log(str);
@@ -191,132 +199,132 @@ const AlertPage = () => {
   };
 
   const sortedFilteredAlerts = filteredAlerts
-    .filter((alert) => {
-      if (
-        selectedProjectId.toString() &&
-        alert.projectId !== selectedProjectId.toString()
-      ) {
-        return false;
-      }
-      const lowerCaseQuery = searchQuery.toLowerCase();
-      const projectAddress =
-        projects.find((p) => p.projectId === alert.projectId)?.address ||
-        "";
+      .filter((alert) => {
+        if (
+            selectedProjectId.toString() &&
+            alert.projectId !== selectedProjectId.toString()
+        ) {
+          return false;
+        }
+        const lowerCaseQuery = searchQuery.toLowerCase();
+        const projectAddress =
+            projects.find((p) => p.projectId === alert.projectId)?.address ||
+            "";
 
-      return (
-        (alert.message?.toLowerCase() || "").includes(lowerCaseQuery) ||
-        alert.unitNumber.toString().includes(lowerCaseQuery) ||
-        alert.type.toLowerCase().includes(lowerCaseQuery) ||
-        projectAddress.toLowerCase().includes(lowerCaseQuery)
-      );
-    })
-    .sort((a, b) => {
-      const projectA =
-        projects.find((p) => p.projectId === a.projectId)?.address || "";
-      const projectB =
-        projects.find((p) => p.projectId === b.projectId)?.address || "";
-      if (filter === " Project A-Z") {
-        return projectA.localeCompare(projectB);
-      } else if (filter === "Project Z-A") {
-        return projectB.localeCompare(projectA);
-      }
+        return (
+            (alert.message?.toLowerCase() || "").includes(lowerCaseQuery) ||
+            alert.unitNumber.toString().includes(lowerCaseQuery) ||
+            alert.type.toLowerCase().includes(lowerCaseQuery) ||
+            projectAddress.toLowerCase().includes(lowerCaseQuery)
+        );
+      })
+      .sort((a, b) => {
+        const projectA =
+            projects.find((p) => p.projectId === a.projectId)?.address || "";
+        const projectB =
+            projects.find((p) => p.projectId === b.projectId)?.address || "";
+        if (filter === " Project A-Z") {
+          return projectA.localeCompare(projectB);
+        } else if (filter === "Project Z-A") {
+          return projectB.localeCompare(projectA);
+        }
 
-      if (filter === "Most Recent") {
-        return (
-          new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime()
-        );
-      } else if (filter === "Least Recent") {
-        return (
-          new Date(a.timestamp).getTime() - new Date(b.timestamp).getTime()
-        );
-      }
-      return 0;
-    });
+        if (filter === "Most Recent") {
+          return (
+              new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime()
+          );
+        } else if (filter === "Least Recent") {
+          return (
+              new Date(a.timestamp).getTime() - new Date(b.timestamp).getTime()
+          );
+        }
+        return 0;
+      });
 
   const totalPages = Math.ceil(sortedFilteredAlerts.length / itemsPerPage);
   const paginatedAlerts = sortedFilteredAlerts.slice(
-    (currentPage - 1) * itemsPerPage,
-    currentPage * itemsPerPage
+      (currentPage - 1) * itemsPerPage,
+      currentPage * itemsPerPage
   );
 
   const handlePageChange = (
-    event: React.ChangeEvent<unknown>,
-    page: number
+      event: React.ChangeEvent<unknown>,
+      page: number
   ) => {
     setCurrentPage(page);
   };
 
   return (
-    <div className="relative mx-4 lg:mx-8 min-h-screen flex-col pb-20">
-      <div className="flex items-center justify-between">
-        <h1 className="w-full text-[#325a67] text-[30px] leading-10 tracking-tight whitespace-nowrap">
-          Alerts
-        </h1>
-        <div className="flex items-center pt-2">
-          <FilterComponent
-            onFilterChange={handleFilterChange}
-            filterOptions={filterOptionsAlerts}
-          />
-          <Searchbar onSearch={handleSearch} />
-        </div>
-      </div>
-      <h2 className="text-left text-[#325a67] text-[16px] leading-2 tracking-tight pb-10">
-        View and manage the alerts generated by the hubs across your
-        organization. Use the Project Filter on the left, or the search bar and
-        filter above to narrow results.{" "}
-      </h2>
-
-      <div className="grid grid-cols-6 gap-x-20 font-semibold border-b-2 border-black pb-2 mb-4">
-        <p className="pl-2 text-[#30525E] text-lg font-sequel-sans-medium leading-tight tracking-tight">
-          Project
-        </p>
-        <p className="pl-2 text-[#30525E] text-lg font-sequel-sans-medium leading-tight tracking-tight">
-          Unit
-        </p>
-        <p className="pl-2 text-[#30525E] text-lg font-sequel-sans-medium leading-tight tracking-tight">
-          Alert
-        </p>
-        <p className="pl-2 text-[#30525E] text-lg font-sequel-sans-medium leading-tight tracking-tight">
-          Type
-        </p>
-        <p className="pl-2 text-[#30525E] text-lg font-sequel-sans-medium leading-tight tracking-tight">
-          Date
-        </p>
-        <p className="pl-2 text-[#30525E] text-lg font-sequel-sans-medium leading-tight tracking-tight">
-          Time
-        </p>
-      </div>
-      
-      {isLoading ? (
-        <div className="flex items-center justify-center h-64">
-          <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900"></div>
-        </div>
-      ) : allAlerts.length === 0 ? (
-        <p className="text-[#729987] text-xl font-sequel-sans-black text-left p-2">
-          No Data Available
-        </p>
-      ) : sortedFilteredAlerts.length === 0 && searchQuery !== "" ? (
-        <div className="unit-container max-w-fit sm:max-w-full mx-auto">
-          <div className="bg-[#fff] rounded-[7px] w-full mt-4 mb-4">
-            <NoResultsFound searchItem={searchQuery} />
+      <div className="relative mx-4 lg:mx-8 min-h-screen flex-col pb-20">
+        <div className="flex items-center justify-between">
+          <h1 className="w-full text-[#325a67] text-[30px] leading-10 tracking-tight whitespace-nowrap">
+            Alerts
+          </h1>
+          <div className="flex items-center pt-2">
+            <FilterComponent
+                onFilterChange={handleFilterChange}
+                filterOptions={filterOptionsAlerts}
+            />
+            <Searchbar onSearch={handleSearch} />
           </div>
         </div>
-      ) : (
-        <AlertList alerts={paginatedAlerts} projects={projects} />
-      )}
-      
-      {!isLoading && allAlerts.length > 0 && sortedFilteredAlerts.length > 0 && (
-        <div className="absolute bottom-0 left-0 w-full bg-white pb-0 flex justify-center">
-          <Pagination
-            className="custom-pagination"
-            count={totalPages}
-            page={currentPage}
-            onChange={handlePageChange}
-            color="primary"
-          />
+        <h2 className="text-left text-[#325a67] text-[16px] leading-2 tracking-tight pb-10">
+          View and manage the alerts generated by the hubs across your
+          organization. Use the Project Filter on the left, or the search bar and
+          filter above to narrow results.{" "}
+        </h2>
+
+        <div className="grid grid-cols-6 gap-x-20 font-semibold border-b-2 border-black pb-2 mb-4">
+          <p className="pl-2 text-[#30525E] text-lg font-sequel-sans-medium leading-tight tracking-tight">
+            Project
+          </p>
+          <p className="pl-2 text-[#30525E] text-lg font-sequel-sans-medium leading-tight tracking-tight">
+            Unit
+          </p>
+          <p className="pl-2 text-[#30525E] text-lg font-sequel-sans-medium leading-tight tracking-tight">
+            Alert
+          </p>
+          <p className="pl-2 text-[#30525E] text-lg font-sequel-sans-medium leading-tight tracking-tight">
+            Type
+          </p>
+          <p className="pl-2 text-[#30525E] text-lg font-sequel-sans-medium leading-tight tracking-tight">
+            Date
+          </p>
+          <p className="pl-2 text-[#30525E] text-lg font-sequel-sans-medium leading-tight tracking-tight">
+            Time
+          </p>
         </div>
-      )}
-    </div>
+
+        {isLoading ? (
+            <div className="flex items-center justify-center h-64">
+              <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900"></div>
+            </div>
+        ) : allAlerts.length === 0 ? (
+            <p className="text-[#729987] text-xl font-sequel-sans-black text-left p-2">
+              No Data Available
+            </p>
+        ) : sortedFilteredAlerts.length === 0 && searchQuery !== "" ? (
+            <div className="unit-container max-w-fit sm:max-w-full mx-auto">
+              <div className="bg-[#fff] rounded-[7px] w-full mt-4 mb-4">
+                <NoResultsFound searchItem={searchQuery} />
+              </div>
+            </div>
+        ) : (
+            <AlertList alerts={paginatedAlerts} projects={projects} />
+        )}
+
+        {!isLoading && allAlerts.length > 0 && sortedFilteredAlerts.length > 0 && (
+            <div className="absolute bottom-0 left-0 w-full bg-white pb-0 flex justify-center">
+              <Pagination
+                  className="custom-pagination"
+                  count={totalPages}
+                  page={currentPage}
+                  onChange={handlePageChange}
+                  color="primary"
+              />
+            </div>
+        )}
+      </div>
   );
 };
 
